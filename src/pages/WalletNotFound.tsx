ccf046602d24e63f87a8b45eafba45e36a9e989f
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-02-27 12:36:52 -0500

    auto format with 4 spaces

--- src/pages/WalletNotFound.tsx
+++ src/pages/WalletNotFound.tsx
@@ -1,36 +1,36 @@
 import { FlexContainer, Paragraph, EmptyConnectionsIcon, Layout } from '@/shared/components';
 
 const WalletNotFound = () => {
-  return (
-    <Layout>
-      <FlexContainer
-        margin='16'
-        minHeight='100%'
-        flexDirection='column'
-        justifyContent='center'
-        alignItems='center'
-      >
-        <FlexContainer
-          maxWidth='210px'
-          flexDirection='column'
-          justifyContent='center'
-          alignItems='center'
-        >
-          <EmptyConnectionsIcon />
-          <Paragraph
-            $typeset='headline'
-            fontWeight='regular'
-            color='base'
-            mt='24'
-            textAlign='center'
-          >
-            You don&apos;t have any wallet imported in ARK Connect! <br />
-            Please create or import a wallet first!
-          </Paragraph>
-        </FlexContainer>
-      </FlexContainer>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <FlexContainer
+                margin='16'
+                minHeight='100%'
+                flexDirection='column'
+                justifyContent='center'
+                alignItems='center'
+            >
+                <FlexContainer
+                    maxWidth='210px'
+                    flexDirection='column'
+                    justifyContent='center'
+                    alignItems='center'
+                >
+                    <EmptyConnectionsIcon />
+                    <Paragraph
+                        $typeset='headline'
+                        fontWeight='regular'
+                        color='base'
+                        mt='24'
+                        textAlign='center'
+                    >
+                        You don&apos;t have any wallet imported in ARK Connect! <br />
+                        Please create or import a wallet first!
+                    </Paragraph>
+                </FlexContainer>
+            </FlexContainer>
+        </Layout>
+    );
 };
 
 export default WalletNotFound;
