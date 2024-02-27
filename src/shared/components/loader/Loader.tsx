874586c1f54263c0a58d2616068ae10d9bf149f5
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-02-27 13:11:46 -0500

    Update Loader.tsx

--- src/shared/components/loader/Loader.tsx
+++ src/shared/components/loader/Loader.tsx
@@ -1,7 +1,5 @@
 import styled, { keyframes } from 'styled-components';
 import { variant as styledSystemVariant } from 'styled-system';
-import useThemeMode from '@/lib/hooks/useThemeMode';
-import { ThemeMode } from '@/lib/store/ui';
 
 type LoaderProps = React.ComponentPropsWithRef<typeof StyledLoader> & {
   variant: LoaderVariant;
