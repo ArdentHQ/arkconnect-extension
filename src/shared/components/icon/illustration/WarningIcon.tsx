1bcbb51bb1b11067883998947ca10ac5e03aa0ea
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-02-28 18:36:58 -0500

    refactor: tailwind classnames for warning icons

--- src/shared/components/icon/illustration/WarningIcon.tsx
+++ src/shared/components/icon/illustration/WarningIcon.tsx
@@ -3,13 +3,11 @@ import useThemeMode from '@/lib/hooks/useThemeMode';
 import { ThemeMode } from '@/lib/store/ui';
 
 export const WarningIcon = ({
-    width = '172px',
-    height = '159px',
+    iconClassName = 'w-[172px] h-[159px]',
 }: {
-    width?: string;
-    height?: string;
+    iconClassName?: string,
 }) => {
     const { currentThemeMode } = useThemeMode();
     const icon = currentThemeMode === ThemeMode.LIGHT ? 'warning-light' : 'warning-dark';
-    return <Icon icon={icon} width={width} height={height} />;
+    return <Icon icon={icon} className={iconClassName} />;
 };
