0d2dcdb913d9c95f87aad5ef319858c2e0d0f217
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-02-29 00:19:02 -0500

    refactor: tailwind for checkbox input

--- src/shared/components/input/Checkbox.tsx
+++ src/shared/components/input/Checkbox.tsx
@@ -1,8 +1,7 @@
 import { FC, ChangeEvent, useState } from 'react';
-import styled from 'styled-components';
-import { FlexContainer, Paragraph } from '@/shared/components';
 import { handleInputKeyAction } from '@/lib/utils/handleKeyAction';
 import { isFirefox } from '@/lib/utils/isFirefox';
+import cn from 'classnames';
 
 type CheckboxProps = {
     checked: boolean;
@@ -14,110 +13,6 @@ type CheckboxProps = {
     disabled?: boolean;
     tabIndex?: number;
 };
-
-const Input = styled.input`
-    height: 0;
-    width: 0;
-    opacity: 0;
-    z-index: -1;
-`;
-
-const Indicator = styled.span<{
-    isFocusWithin?: boolean;
-}>`
-    ${({ theme }) => `
-    width: 20px;
-    height: 20px;
-    background-color: transparent;
-    position: absolute;
-    top: 0em;
-    border: 1px solid ${theme.colors.primary};
-    border-radius: 6px;
-    transition: ${isFirefox ? theme.transitions.firefoxSmoothEase : theme.transitions.smoothEase};
-
-    &:hover {
-      background-color: ${theme.colors.inputHover};
-    }
-
-    ${Input}:not(:disabled):checked + & {
-      background-color: ${theme.colors.primary};
-
-      &:hover {
-        background-color: ${theme.colors.checkboxBackground};
-
-        &::after {
-          border: solid ${theme.colors.primary};
-          border-width: 0 0.15em 0.15em 0;
-        }
-      }
-    }
-
-    &::after {
-      content: '';
-      position: absolute;
-      display: none;
-    }
-
-    ${Input}:checked + &::after {
-      transform: rotate(45deg);
-      display: block;
-      top: 0.1em;
-      left: 0.35em;
-      width: 35%;
-      height: 70%;
-      border: solid ${theme.colors.background};
-      border-width: 0 0.2em 0.2em 0;
-    }
-
-    &:disabled {
-      cursor: not-allowed;
-    }
-
-    ${Input}:disabled + & {
-      border: 1px solid ${theme.colors.secondary300};
-      background-color: ${theme.colors.disabledCheckbox};
-      &::after {
-        border: solid ${theme.colors.disabledRadio};
-        border-width: 0 0.2em 0.2em 0;
-      }
-    }
-  `}
-
-    ${({ isFocusWithin, theme }) => `
-    ${
-        isFocusWithin &&
-        `
-        outline-color: ${theme.colors.primary600};
-        outline-style: solid;
-        outline-width: 2px;
-        outline-offset: 2px;
-      `
-    }
-  `}
-`;
-
-const Label = styled.label<{ disabled?: boolean }>`
-    position: relative;
-    display: inline-block;
-    cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
-    pointer-events: ${(props) => (props.disabled ? 'none' : 'auto')};
-    display: flex;
-
-    ${({ theme }) => `
-    &:hover ${Indicator} {
-      background-color: ${theme.colors.inputHover};
-    }
-
-    &:hover ${Input}:not(:disabled):checked + ${Indicator} {
-      background-color: ${theme.colors.checkboxBackground};
-
-      &::after {
-        border: solid ${theme.colors.primary};
-        border-width: 0 0.15em 0.15em 0;
-      }
-    }
-  `}
-`;
 
 export const Checkbox: FC<CheckboxProps> = ({
     checked,
@@ -132,51 +27,58 @@ export const Checkbox: FC<CheckboxProps> = ({
     const [isFocusWithin, setIsFocusWithin] = useState(false);
 
     return (
-        <Label htmlFor={id} disabled={disabled}>
-            <FlexContainer
-                flexDirection='column'
-                alignItems='flex-start'
-                gridGap='5px'
-                marginLeft='28'
-                as='span'
-            >
-                {title && (
-                    <Paragraph $typeset='headline' color='base'>
-                        {title}
-                    </Paragraph>
-                )}
-                {helperText && (
-                    <Paragraph $typeset='body' fontWeight='regular' color='gray' as='span'>
-                        {helperText}
-                    </Paragraph>
-                )}
-            </FlexContainer>
+      <label htmlFor={id} className={cn('relative flex group', {
+        'cursor-not-allowed pointer-events-none': disabled,
+        'cursor-pointer pointer-events-auto': !disabled,
+      })}>
+        <span className='flex flex-col items-start gap-[5px] ml-7'>
+          {
+            title && (
+              <p className='text-base font-normal leading-tight text-light-black dark:text-white'>
+                {title}
+              </p>
+            )
+          }
+          {
+            helperText && (
+              <span className='text-sm font-normal leading-tight text-theme-secondary-600 dark:text-theme-secondary-300'>
+                {helperText}
+              </span>
+            )
+          }
+        </span>
 
-            <Input
-                id={id}
-                type='checkbox'
-                name={name}
-                disabled={disabled}
-                checked={!!checked}
-                onChange={onChange}
-                tabIndex={tabIndex}
-                onFocus={(event) => {
-                    setIsFocusWithin(event.relatedTarget !== null);
-                }}
-                onBlur={() => {
-                    setIsFocusWithin(false);
-                }}
-            />
-            <Indicator
-                role='checkbox'
-                aria-label={`${title} checkbox`}
-                aria-checked={checked}
-                aria-disabled={disabled}
-                onKeyDown={(e) =>
-                    handleInputKeyAction(e, onChange, e as unknown as ChangeEvent<HTMLInputElement>)
-                }
-                isFocusWithin={isFocusWithin}
-            />
-        </Label>
+        <input
+          id={id}
+          type='checkbox'
+          name={name}
+          disabled={disabled}
+          checked={checked}
+          onChange={onChange}
+          tabIndex={tabIndex}
+          onFocus={(event) => {
+            setIsFocusWithin(event.relatedTarget !== null);
+          }}
+          onBlur={() => {
+            setIsFocusWithin(false);
+          }}
+          className='h-0 w-0 opacity-0 z-[-1]'
+        />
+        <div
+          role='checkbox'
+          aria-label={`${title} checkbox`}
+          aria-checked={checked}
+          aria-disabled={disabled}
+          onKeyDown={(e) =>
+              handleInputKeyAction(e, onChange, e as unknown as ChangeEvent<HTMLInputElement>)
+          }
+          className={cn('checkbox-indicator', {
+            'transition-firefoxSmoothEase': isFirefox,
+            'transition-smoothEase': !isFirefox,
+            'outline outline-2 outline-offset-2 outline-theme-primary-600': isFocusWithin,
+            'cursor-not-allowed': disabled,
+          })}
+        />
+      </label>
     );
 };
