010c34831c648cb782387ae07698ffbc948d47bc
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-02-29 04:27:50 -0500

    refactor: cache coingecko response (#8)

--- src/lib/hooks/useExchangeRates.ts
+++ src/lib/hooks/useExchangeRates.ts
@@ -1,26 +1,46 @@
 import { useEffect, useState } from 'react';
+import { getLocalValues, setLocalValue } from '@/lib/utils/localStorage';
 import { general } from '@/lib/data/general';
+
+const ONE_MINUTE_IN_MS = 60000;
+const COINGECKO_API_URL = 'https://api.coingecko.com/api/v3/simple/price';
+const COIN_ID = 'ark';
+
+const fetchRates = async () => {
+    const url = new URL(COINGECKO_API_URL);
+    url.searchParams.set('ids', COIN_ID);
+    url.searchParams.set(
+        'vs_currencies',
+        general.currencies.map((currency) => currency.value).join(','),
+    );
+
+    const response = await fetch(url);
+    const parsedRates = await response.json();
+    return parsedRates[COIN_ID];
+};
 
 export const useExchangeRates = () => {
     const [rates, setRates] = useState<Record<string, number>>({});
     const [isLoading, setLoading] = useState<boolean>(true);
 
-    const coinId = 'ark';
-    const coingeckoApiUrl = 'https://api.coingecko.com/api/v3/simple/price';
-
     useEffect(() => {
-        const url = new URL(coingeckoApiUrl);
-        url.searchParams.set('ids', coinId);
-        url.searchParams.set(
-            'vs_currencies',
-            general.currencies.map((currency) => currency.value).join(','),
-        );
-
         const getRates = async () => {
-            const rates = await fetch(url);
-            const parsedRates = await rates.json();
+            const now = Date.now();
+            const { ratesCache } = await getLocalValues();
 
-            setRates(parsedRates[coinId]);
+            if (ratesCache === undefined || now - ratesCache.lastFetch >= ONE_MINUTE_IN_MS) {
+                const rates = await fetchRates();
+
+                await setLocalValue('ratesCache', {
+                    lastFetch: now,
+                    rates: rates,
+                });
+
+                setRates(rates);
+            } else {
+                setRates(ratesCache.rates);
+            }
+
             setLoading(false);
         };
 
