02faff7b5e5792a4a91a4a2c0cb9bfd92fce2506
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-02-29 12:55:44 -0600

    ensure it removes the profiles

--- src/lib/background/extension.ts
+++ src/lib/background/extension.ts
@@ -1,7 +1,7 @@
 import { Contracts, Environment } from '@ardenthq/sdk-profiles';
+import { initializeEnvironment } from '../utils/env.background';
 import { LockHandler } from './handleAutoLock';
 import { PrimaryWallet } from './extension.wallet.primary';
-import { initializeEnvironment } from '../utils/env.background';
 
 const exists = (profile?: Contracts.IProfile | null): profile is Contracts.IProfile => !!profile;
 
@@ -94,6 +94,8 @@ export function Extension() {
             }
 
             env.profiles().flush();
+
+            await env.persist();
 
             const profile = await env.profiles().create('arkconnect');
             profile.auth().setPassword(password);
