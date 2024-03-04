755ae1a32777cceab218915f60f327ecdeed64bc
Patricio Marroquin <55117912+patricio0312rev@users.noreply.github.com>
2024-03-04 06:24:52 -0500

    refactor: tailwind for radio toggle input (#47)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    content: ['./src/**/*.{js,ts,jsx,tsx,css}'],
     darkMode: 'class',
     theme: {
         extend: {
