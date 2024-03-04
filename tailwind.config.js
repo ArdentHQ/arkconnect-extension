a7760c44078f92806edc0d87519dc35d4f6cc6c6
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-03-04 11:17:11 +0000

    style: resolve style guide violations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{js,ts,jsx,tsx,css}"],
+    content: ['./src/**/*.{js,ts,jsx,tsx,css}'],
     darkMode: 'class',
     theme: {
         extend: {
