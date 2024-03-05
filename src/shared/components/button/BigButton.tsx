d53416edaee97ad89c0da808fcb7d4c96febe4da
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-03-05 05:33:56 +0000

    style: resolve style guide violations

--- src/shared/components/button/BigButton.tsx
+++ src/shared/components/button/BigButton.tsx
@@ -18,32 +18,32 @@ export const BigButton = ({
     className,
     ...rest
 }: BigButtonProps) => {
-
     return (
         <button
-            className={cn('border border-solid border-transparent rounded-[20px] bg-white dark:bg-subtle-black shadow-[0_1px_4px_0_rgba(0,0,0,0.05)] flex w-full p-4 max-h-24 box-border cursor-pointer gap-3 hover:border hover:border-solid hover:border-theme-primary-800 hover:dark:border-theme-primary-600 disabled:cursor-not-allowed disabled:pointer-events-none', {
-                'transition-firefoxSmoothEase focus-visible:outline focus-visible:outline-2': isFirefox,
-                'transition-smoothEase': !isFirefox,
-            }, className)}
+            className={cn(
+                'border border-solid border-transparent rounded-[20px] bg-white dark:bg-subtle-black shadow-[0_1px_4px_0_rgba(0,0,0,0.05)] flex w-full p-4 max-h-24 box-border cursor-pointer gap-3 hover:border hover:border-solid hover:border-theme-primary-800 hover:dark:border-theme-primary-600 disabled:cursor-not-allowed disabled:pointer-events-none',
+                {
+                    'transition-firefoxSmoothEase focus-visible:outline focus-visible:outline-2':
+                        isFirefox,
+                    'transition-smoothEase': !isFirefox,
+                },
+                className,
+            )}
             {...rest}
         >
             <span className='flex w-full gap-3 items-start'>
                 {iconLeading && <Icon className='h-6 w-6' icon={iconLeading} />}
-                
+
                 <span className='flex items-center justify-between w-full'>
                     <span className='flex flex-col items-start w-full gap-2'>
-                        {title && (
-                            <span className='typeset-heading font-medium'>{title}</span>
-                        )}
+                        {title && <span className='typeset-heading font-medium'>{title}</span>}
                         {helperText && (
                             <span className='typeset-body text-left font-normal'>{helperText}</span>
                         )}
                     </span>
-                   
+
                     <span className='h-full flex flex-col items-center justify-center text-light-black dark:text-white'>
-                        {iconTrailing && (
-                            <Icon className='h-5 w-5' icon={iconTrailing} />
-                        )}
+                        {iconTrailing && <Icon className='h-5 w-5' icon={iconTrailing} />}
                     </span>
                 </span>
             </span>
