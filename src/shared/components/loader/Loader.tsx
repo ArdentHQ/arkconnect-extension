3971d8b46ab34fd6b20a2fccbabee3953e43e71b
Patricio Marroquin <55117912+patricio0312rev@users.noreply.github.com>
2024-03-05 12:37:47 -0500

    Update src/shared/components/loader/Loader.tsx
    Co-authored-by: ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>

--- src/shared/components/loader/Loader.tsx
+++ src/shared/components/loader/Loader.tsx
@@ -10,11 +10,11 @@ export const Loader = ({ variant, ...rest }: LoaderProps) => {
     return (
         <span
             className={cn('rounded-full animate-spin border-solid', {
-                'lw-6 h-6 border-2 border-solid border-transparent border-t-white':
+                'w-6 h-6 border-2 border-transparent border-t-white':
                     variant === 'small',
-                'h-16 w-16 border-[6px] border-solid border-theme-secondary-100 dark:border-subtle-black border-t-theme-primary-700 dark:border-t-theme-primary-650':
+                'h-16 w-16 border-[6px] border-theme-secondary-100 dark:border-subtle-black border-t-theme-primary-700 dark:border-t-theme-primary-650':
                     variant === 'big',
-                'w-4.5 h-4.5 border-2 border-solid border-theme-warning-200 border-t-theme-warning-500':
+                'w-4.5 h-4.5 border-2 border-theme-warning-200 border-t-theme-warning-500':
                     variant === 'warning',
             })}
             {...rest}
