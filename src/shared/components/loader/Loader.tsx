7ce4616529e8f8669923bcdd6d50cc82ffc4ac97
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-03-05 17:39:21 +0000

    style: resolve style guide violations

--- src/shared/components/loader/Loader.tsx
+++ src/shared/components/loader/Loader.tsx
@@ -10,8 +10,7 @@ export const Loader = ({ variant, ...rest }: LoaderProps) => {
     return (
         <span
             className={cn('rounded-full animate-spin border-solid', {
-                'w-6 h-6 border-2 border-transparent border-t-white':
-                    variant === 'small',
+                'w-6 h-6 border-2 border-transparent border-t-white': variant === 'small',
                 'h-16 w-16 border-[6px] border-theme-secondary-100 dark:border-subtle-black border-t-theme-primary-700 dark:border-t-theme-primary-650':
                     variant === 'big',
                 'w-4.5 h-4.5 border-2 border-theme-warning-200 border-t-theme-warning-500':
