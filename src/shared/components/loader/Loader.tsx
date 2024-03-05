11ec88e46aa70a73d8363c1cbcebb482fec2f80c
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-03-05 09:38:19 -0500

    refactor: tailwind for loader

--- src/shared/components/loader/Loader.tsx
+++ src/shared/components/loader/Loader.tsx
@@ -1,50 +1,17 @@
-import styled, { keyframes } from 'styled-components';
-import { variant as styledSystemVariant } from 'styled-system';
+import cn from 'classnames';
 
-type LoaderProps = React.ComponentPropsWithRef<typeof StyledLoader> & {
+type LoaderProps = React.ComponentPropsWithRef<'span'> & {
     variant: LoaderVariant;
 };
 
 type LoaderVariant = 'small' | 'big' | 'warning';
 
 export const Loader = ({ variant, ...rest }: LoaderProps) => {
-    return <StyledLoader variant={variant} {...rest} />;
+    return (
+        <span className={cn('rounded-full animate-spin', {
+            'loader-small': variant === 'small',
+            'loader-big': variant === 'big',
+            'loader-warning': variant === 'warning',
+        })} {...rest}></span>
+    );
 };
-
-const spin = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-`;
-
-const StyledLoader = styled.div<{ variant: LoaderVariant }>`
-    border-radius: 50%;
-    animation: ${spin} 1s linear infinite;
-
-    ${({ theme }) =>
-        styledSystemVariant({
-            variants: {
-                small: {
-                    width: '24px',
-                    height: '24px',
-                    border: '2px solid transparent',
-                    borderTop: `2px solid ${theme.colors.white}`,
-                },
-                big: {
-                    width: '64px',
-                    height: '64px',
-                    border: `6px solid ${theme.colors.loaderBackground}`,
-                    borderTop: `6px solid ${theme.colors.primary}`,
-                },
-                warning: {
-                    width: '18px',
-                    height: '18px',
-                    border: `2px solid ${theme.colors.warning200}`,
-                    borderTop: `2px solid ${theme.colors.warning500}`,
-                },
-            },
-        })};
-`;
