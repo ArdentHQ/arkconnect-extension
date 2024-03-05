2149c53c1d4f428ba861f059f706bfb5716fc1d5
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-03-05 17:16:36 +0000

    style: resolve style guide violations

--- src/shared/components/input/PassphraseInput.tsx
+++ src/shared/components/input/PassphraseInput.tsx
@@ -10,12 +10,22 @@ type TextAreaProps = React.ComponentPropsWithRef<'textarea'> & {
     variant?: 'primary' | 'destructive' | 'errorFree';
 };
 
-
 export const PassphraseInput = forwardRef(function TextArea(
     props: TextAreaProps,
     ref: React.Ref<HTMLTextAreaElement>,
 ) {
-    const { labelText, helperText, variant, id, rows, value, onChange, className, hideValue, ...rest } = props;
+    const {
+        labelText,
+        helperText,
+        variant,
+        id,
+        rows,
+        value,
+        onChange,
+        className,
+        hideValue,
+        ...rest
+    } = props;
 
     const handleOnChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         if (onChange) {
@@ -33,15 +43,19 @@ export const PassphraseInput = forwardRef(function TextArea(
                     {labelText}
                 </label>
             )}
-            
+
             <div className='relative w-full'>
                 <textarea
-                    className={cn('text-base font-normal w-full p-3 rounded-lg shadow-[0_1px_2px_0_rgba(16,24,40,0.05)] transition-smoothEase border-none outline-none resize-none disabled:cursor-not-allowed disabled:pointer-events-none placeholder:text-theme-secondary-400', {
-                        'passphrase-primary': variant === 'primary',
-                        'passphrase-destructive': variant === 'destructive',
-                        'passphrase-errorFree': variant === 'errorFree',
-                        'text-security-disc': hideValue,
-                    }, className)}
+                    className={cn(
+                        'text-base font-normal w-full p-3 rounded-lg shadow-[0_1px_2px_0_rgba(16,24,40,0.05)] transition-smoothEase border-none outline-none resize-none disabled:cursor-not-allowed disabled:pointer-events-none placeholder:text-theme-secondary-400',
+                        {
+                            'passphrase-primary': variant === 'primary',
+                            'passphrase-destructive': variant === 'destructive',
+                            'passphrase-errorFree': variant === 'errorFree',
+                            'text-security-disc': hideValue,
+                        },
+                        className,
+                    )}
                     rows={rows || 4}
                     ref={ref}
                     id={id}
@@ -52,10 +66,13 @@ export const PassphraseInput = forwardRef(function TextArea(
             </div>
 
             {helperText && (
-                <p className={cn('typeset-body', {
-                    'text-theme-error-600 dark:text-theme-error-500': variant === 'destructive',
-                    'text-theme-secondary-500 dark:text-theme-secondary-300': variant !== 'destructive',
-                })}>
+                <p
+                    className={cn('typeset-body', {
+                        'text-theme-error-600 dark:text-theme-error-500': variant === 'destructive',
+                        'text-theme-secondary-500 dark:text-theme-secondary-300':
+                            variant !== 'destructive',
+                    })}
+                >
                     {helperText}
                 </p>
             )}
