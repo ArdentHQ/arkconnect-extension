0a417fbf054bfdc4fa8f57217d44f124ae0a7ccd
Shahin Safaraliyev <shahin@ardenthq.com>
2024-03-06 16:36:21 +0400

    fix: locking extension during wallet creation

--- src/lib/background/eventListenerHandlers.ts
+++ src/lib/background/eventListenerHandlers.ts
@@ -127,7 +127,7 @@ const handleOnConnect = async (
     profile: Contracts.IProfile | null,
 ) => {
     try {
-        if (profile?.wallets().count() !== 0) {
+        if (profile?.wallets().count() === 0) {
             void initWindow(payload);
 
             throw new Error('No profile found. Please connect your wallet and try again.');
