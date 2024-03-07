67915ab6c53b5622a9804a302e978e2a0a2bf527
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-03-07 11:58:05 -0500

    refactor: tailwind for toast component

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -1,22 +1,15 @@
-import { FlexContainer, Icon, IconDefinition, Paragraph } from '@/shared/components';
+import { Icon, IconDefinition } from '@/shared/components';
 import * as UIStore from '@/lib/store/ui';
 
 const Toast = ({ type, message }: UIStore.Toast) => {
+
     return (
-        <FlexContainer
-            display='flex'
-            alignItems='center'
-            paddingX='10'
-            paddingY='8'
-            backgroundColor='base'
-            borderRadius='8'
-            marginTop='8'
-        >
+        <div className='flex items-center px-2.5 py-2 bg-light-black dark:bg-white rounded-lg mt-2'>
             <Icon icon={type as IconDefinition} className='h-5 w-5' />
-            <Paragraph $typeset='body' fontWeight='regular' color='background' marginLeft='4'>
+            <p className='typeset-body font-normal text-white dark:text-light-black ml-1 w-fit'>
                 {message}
-            </Paragraph>
-        </FlexContainer>
+            </p>
+        </div>
     );
 };
 
