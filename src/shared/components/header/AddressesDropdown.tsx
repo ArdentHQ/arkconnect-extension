2c27382d04da461d241aec8c9575f3b998093d43
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-03-06 18:13:26 -0600

    replace container

--- src/shared/components/header/AddressesDropdown.tsx
+++ src/shared/components/header/AddressesDropdown.tsx
@@ -94,14 +94,7 @@ export const AddressesDropdown = ({
     };
 
     return (
-        <Container
-            borderRadius='12'
-            boxShadow='0 4px 6px -2px rgba(16, 24, 40, 0.03), 0 12px 16px -4px rgba(16, 24, 40, 0.08)'
-            marginX='16'
-            backgroundColor='secondaryBackground'
-            width='100%'
-            ref={dropdownRef}
-        >
+        <div className=' mx-4 w-full rounded-xl shadow-dropdown bg-white dark:bg-subtle-black'>
             <Container borderBottom='1px solid' borderBottomColor='toggleInactive'>
                 <FlexContainer padding='12' justifyContent='space-between' alignItems='center'>
                     <Paragraph $typeset='headline' fontWeight='medium' color='base'>
@@ -140,7 +133,7 @@ export const AddressesDropdown = ({
                     />
                 ))}
             </FlexContainer>
-        </Container>
+        </div>
     );
 };
 
