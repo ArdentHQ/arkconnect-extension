69c104f1123d84142924418b99dc7f2e86622d4e
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-03-07 16:11:01 -0600

    refactor: use tailwindcss on home page (#109)

--- src/shared/components/header/HeaderWrapper.tsx
+++ src/shared/components/header/HeaderWrapper.tsx
@@ -13,7 +13,7 @@ export const HeaderWrapper = ({ withShadow, className, ...properties }: HeaderWr
                 cn(
                     'fixed left-0 top-0 z-20 flex w-full items-center justify-center bg-white px-0 py-3 dark:bg-subtle-black',
                     {
-                        'shadow-header dark:shadow-header-dark': withShadow,
+                        'shadow-light dark:shadow-dark': withShadow,
                     },
                 ),
                 className,
