af61b097ab1b1c9f0b670a0bd685ebf681cb42a5
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-03-07 17:48:29 +0000

    style: resolve style guide violations

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ export default {
             boxShadow: {
                 dropdown:
                     '0 4px 6px -2px rgba(16, 24, 40, 0.03), 0 12px 16px -4px rgba(16, 24, 40, 0.08)',
-            }
+            },
         },
         colors: {
             transparent: 'transparent',
