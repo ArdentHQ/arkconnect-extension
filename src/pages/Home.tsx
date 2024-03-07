1da978c162813d93300f7665f5db4712b9be87a5
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-03-07 13:36:23 -0600

    use tailwindcss on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import Balance from '@/components/wallet/Balance';
-import { ExternalLink, FlexContainer, Icon, Layout, Paragraph } from '@/shared/components';
+import { ExternalLink, Icon, Layout } from '@/shared/components';
 import constants from '@/constants';
 import { clearPersistScreenData } from '@/components/wallet/form-persist/helpers';
 import { useProfileContext } from '@/lib/context/Profile';
@@ -30,6 +30,7 @@ const Home = () => {
                             convertedBalance={convertedBalance}
                         />
                     </div>
+
                     <div className='flex justify-between border-t border-solid border-t-theme-primary-650 p-4 dark:border-t-theme-primary-600'>
                         <ExternalLink
                             gridGap='8px'
@@ -43,11 +44,11 @@ const Home = () => {
                                       }/${primaryWallet?.address()}`
                             }
                         >
-                            <Paragraph $typeset='body' fontWeight='medium'>
-                                Explorer
-                            </Paragraph>
+                            <span className='text-sm font-medium'>Explorer</span>
+
                             <Icon icon='link-external' className='h-4 w-4' />
                         </ExternalLink>
+
                         <Icon
                             icon='divider'
                             className='h-4.5 w-0.5 text-theme-primary-650 dark:text-theme-primary-600'
@@ -63,31 +64,25 @@ const Home = () => {
                                     : constants.ARKSCAN_EXCHANGES
                             }
                         >
-                            <Paragraph $typeset='body' fontWeight='medium'>
+                            <span className='text-sm font-medium'>
                                 {primaryWallet?.network().isTest() ? 'Faucet' : 'Exchanges'}
-                            </Paragraph>
+                            </span>
                             <Icon icon='link-external' className='h-4 w-4' />
                         </ExternalLink>
                     </div>
                 </div>
+
                 <ExternalLink
                     href={constants.ARK_CONNECT_DEMO}
                     color={getThemeColor('primary', 'primary600')}
                     display='block'
                     borderRadius='16'
                 >
-                    <FlexContainer
-                        padding='16'
-                        flexDirection='row'
-                        justifyContent='space-between'
-                        alignItems='center'
-                    >
+                    <div className='flex flex-row items-center justify-between p-4'>
                         <Icon icon='speakerphone' className='h-4.5 w-4.5' />
-                        <Paragraph $typeset='body' fontWeight='medium'>
-                            Try Our Demo App Now!
-                        </Paragraph>
+                        <span className=' text-sm font-medium'>Try Our Demo App Now!</span>
                         <Icon icon='link-external' className='h-4 w-4' />
-                    </FlexContainer>
+                    </div>
                 </ExternalLink>
             </div>
         </Layout>
