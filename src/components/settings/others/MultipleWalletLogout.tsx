afb695dc2c43cffa104e7f1c9933ed4c1e72dcb2
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-03-11 20:01:19 +0000

    style: resolve style guide violations

--- src/components/settings/others/MultipleWalletLogout.tsx
+++ src/components/settings/others/MultipleWalletLogout.tsx
@@ -58,7 +58,7 @@ const MultipleWalletLogout = () => {
                             }
                             address={wallet.address()}
                             helperText={generateWalletHelperText(wallet, false)}
-                            className='mb-2 group'
+                            className='group mb-2'
                             testnetIndicator={wallet.network().isTest()}
                             ledgerIndicator={wallet.isLedger()}
                             onClick={(evt) => handleSelectWallet(evt, wallet.id())}
