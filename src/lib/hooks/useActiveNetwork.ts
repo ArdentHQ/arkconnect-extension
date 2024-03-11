e0c64e554e8c9b3728883df2f505c51486f828a7
Shahin Safaraliyev <shahin@ardenthq.com>
2024-03-11 14:48:23 +0400

    fix: persist network

--- src/lib/hooks/useActiveNetwork.ts
+++ src/lib/hooks/useActiveNetwork.ts
@@ -2,20 +2,29 @@ import {useLocation} from 'react-router-dom';
 import {WalletNetwork} from '@/lib/store/wallet';
 import {useProfileContext} from '@/lib/context/Profile';
 import {getPersistedValues} from "@/components/wallet/form-persist";
+import {LastVisitedPage, ProfileData} from "@/lib/background/contracts";
 
 const useActiveNetwork = () => {
     const { profile } = useProfileContext();
 
     const { state } = useLocation();
 
-    const {persistScreen} = getPersistedValues();
-
     const networks = profile.availableNetworks();
 
     let selectedNetwork = WalletNetwork.MAINNET;
 
-    if (persistScreen && persistScreen?.step > 0) {
-       selectedNetwork = persistScreen.networkName === WalletNetwork.MAINNET ? WalletNetwork.MAINNET : WalletNetwork.DEVNET;
+    const lastVisitedPage = profile.settings().get(ProfileData.LastVisitedPage) as
+        | LastVisitedPage
+        | undefined;
+
+    if (lastVisitedPage && lastVisitedPage.data.network) {
+        selectedNetwork = lastVisitedPage.data.network === "ark.devnet" ? WalletNetwork.DEVNET : WalletNetwork.MAINNET;
+    }
+
+    const {persistScreen} = getPersistedValues();
+
+    if (persistScreen && persistScreen?.networkName) {
+       selectedNetwork = persistScreen.networkName === WalletNetwork.DEVNET ? WalletNetwork.DEVNET: WalletNetwork.MAINNET;
     }
 
     if (state?.isTestnet) {
@@ -25,7 +34,6 @@ const useActiveNetwork = () => {
     if (profile.wallets().count() === 0) {
         selectedNetwork = WalletNetwork.MAINNET;
     }
-
 
     return networks.find((n) => n.name() === selectedNetwork) || networks[0];
 };
