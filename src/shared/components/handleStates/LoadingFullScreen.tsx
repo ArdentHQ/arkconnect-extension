6c8e9aea9b8a941a259a96c265e4444637f2bb88
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-03-12 09:48:07 -0600

    use state for dark color on loading full screen

--- src/shared/components/handleStates/LoadingFullScreen.tsx
+++ src/shared/components/handleStates/LoadingFullScreen.tsx
@@ -1,8 +1,17 @@
+import classNames from 'classnames';
 import { Loader } from '../loader/Loader';
+import useThemeMode from '@/lib/hooks/useThemeMode';
 
 export const LoadingFullScreen = () => {
+    const { isDark } = useThemeMode();
+
     return (
-        <div className='flex h-screen w-screen items-center justify-center bg-subtle-white dark:bg-light-black'>
+        <div
+            className={classNames('flex h-screen w-screen items-center justify-center', {
+                'bg-light-black': isDark,
+                'bg-subtle-white': !isDark,
+            })}
+        >
             <Loader variant='big' />
         </div>
     );
