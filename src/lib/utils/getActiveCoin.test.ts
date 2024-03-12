7b3185d73eb7fe37a1d7268eee1e0638f4c62997
goga-m <goga.mamoulasvili@gmail.com>
2024-03-12 14:26:24 +0100

    Revert "wip"
    This reverts commit b7c9de61a4991d4001178db4a8c388437621c1da.

--- src/lib/utils/getActiveCoin.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest';
-import getActiveCoin from './getActiveCoin';
-
-describe('getActiveCoin', () => {
-    it('should get mainnet if network is not provided', () => {
-        expect(getActiveCoin()).toBe('ARK');
-    });
-});
