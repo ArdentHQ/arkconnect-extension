e8c8b1930eba12a8cb3cf6e5596f5a19fa25c78c
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-03-12 10:41:15 -0600

    explain the reason for manually dark mode

--- src/shared/components/handleStates/LoadingFullScreen.tsx
+++ src/shared/components/handleStates/LoadingFullScreen.tsx
@@ -8,6 +8,9 @@ export const LoadingFullScreen = () => {
     return (
         <div
             className={classNames('flex h-screen w-screen items-center justify-center', {
+                // The loading screen is the first thing to show when the extension is loaded,
+                // which means we can't rely on the dark class initially since the ThemeProvider
+                // adds the class after is mounted. We need to check the theme mode manually.
                 'bg-light-black': isDark(),
                 'bg-subtle-white': !isDark(),
             })}
