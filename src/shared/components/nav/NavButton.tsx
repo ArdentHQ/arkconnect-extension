cdaff40a2427c82c63e7cc1f2c95cceee263a5e1
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-03-12 13:22:31 -0500

    fix: restore 50px for rounded values

--- src/shared/components/nav/NavButton.tsx
+++ src/shared/components/nav/NavButton.tsx
@@ -7,7 +7,7 @@ export const NavButton = ({ className, ...properties }: Properties) => {
     return (
         <button
             className={cn(
-                'rounded-12.5 p-1.75 enabled:hover:bg-theme-secondary-100 dark:enabled:hover:bg-theme-secondary-700',
+                'rounded-[50px] p-1.75 enabled:hover:bg-theme-secondary-100 dark:enabled:hover:bg-theme-secondary-700',
                 {
                     'transition-firefoxSmoothEase': isFirefox,
                     'transition-smoothEase': !isFirefox,
