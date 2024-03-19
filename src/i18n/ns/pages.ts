f5c4455fb2fe74a34d91ceb73cb0acab7f7dc160
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-03-19 12:09:51 -0600

    wip

--- src/i18n/ns/pages.ts
+++ src/i18n/ns/pages.ts
@@ -2,4 +2,14 @@ export default {
     HOME: {
         TRY_OUR_DEMO_APP_NOW: 'Try Our Demo App Now!',
     },
+    CONNECT: {
+        CONNECT_TO_APP: 'Connect to App',
+        FEEDBACK: {
+            ALREADY_CONNECTED: 'Already connected!',
+            WALLET_NOT_FOUND: 'Wallet not found',
+            CONNECTION_DENIED: 'Connection denied!',
+            CONNECTING: 'Connecting...',
+            SECURELY_CONNECTED: 'Securely Connected',
+        },
+    },
 };
