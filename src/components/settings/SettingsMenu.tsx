8fd4be85260adaeaf527674279f3d0c78da5d9e4
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-03-21 03:52:25 -0600

    refactor: missing i18n translations (#189)

--- src/components/settings/SettingsMenu.tsx
+++ src/components/settings/SettingsMenu.tsx
@@ -2,6 +2,7 @@ import { NavigateOptions, useLocation, useNavigate } from 'react-router-dom';
 import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { runtime } from 'webextension-polyfill';
 import { Contracts } from '@ardenthq/sdk-profiles';
+import { useTranslation } from 'react-i18next';
 import { ToggleSwitch } from '@/shared/components';
 import { SettingsOption } from '@/components/settings/SettingsOption';
 import { lockedChanged } from '@/lib/store/ui';
@@ -25,6 +26,7 @@ export const SettingsMenu = ({
     onClose: () => void;
     triggerRef: React.RefObject<HTMLElement | null>;
 }) => {
+    const { t } = useTranslation();
     const dispatch = useAppDispatch();
     const walletsIds = useAppSelector(selectWalletsIds);
     const { toggleThemeMode, isDark } = useThemeMode();
@@ -75,7 +77,7 @@ export const SettingsMenu = ({
             <SafeOutlineOverflowContainer className='ml-0 w-full px-0'>
                 <div className='flex w-full flex-col py-2'>
                     <SettingsOption
-                        title='Create & Import Address'
+                        title={t('PAGES.SETTINGS.MENU.CREATE_N_IMPORT_ADDRESS')}
                         iconLeading='plus-circle'
                         onClick={() => {
                             handleNavigation('/create-import-address');
@@ -88,7 +90,7 @@ export const SettingsMenu = ({
                         }
                     />
                     <SettingsOption
-                        title='Connected Apps'
+                        title={t('PAGES.SETTINGS.MENU.CONNECTED_APPS')}
                         iconLeading='app'
                         onClick={() => handleNavigation('/connections')}
                         iconTrailing='arrow-right'
@@ -97,13 +99,13 @@ export const SettingsMenu = ({
                         }
                     />
                     <SettingsOption
-                        title='Lock Extension'
+                        title={t('PAGES.SETTINGS.MENU.LOCK_EXTENSION')}
                         iconLeading='lock'
                         onClick={lockExtension}
                         onKeyDown={(e) => handleSubmitKeyAction(e, lockExtension)}
                     />
                     <SettingsOption
-                        title='Dark Mode'
+                        title={t('PAGES.SETTINGS.MENU.DARK_MODE')}
                         iconLeading='moon'
                         iconClassName='text-light-black'
                         onClick={(evt) => toggleThemeMode(evt)}
@@ -125,7 +127,7 @@ export const SettingsMenu = ({
                         }
                     />
                     <SettingsOption
-                        title='Change Password'
+                        title={t('PAGES.SETTINGS.MENU.CHANGE_PASSWORD')}
                         iconLeading='shield-border'
                         iconTrailing='arrow-right'
                         iconClassName='text-light-black'
@@ -135,7 +137,7 @@ export const SettingsMenu = ({
                         }
                     />
                     <SettingsOption
-                        title='Change Local Currency'
+                        title={t('PAGES.SETTINGS.MENU.CHANGE_LOCAL_CURRENCY')}
                         iconLeading='currency-dollar-circle'
                         rightContent={
                             <p className='typeset-headline mr-2 text-base font-normal text-theme-secondary-500 dark:text-theme-secondary-300'>
@@ -151,7 +153,7 @@ export const SettingsMenu = ({
                         }
                     />
                     <SettingsOption
-                        title='Auto Lock Timer'
+                        title={t('PAGES.SETTINGS.MENU.AUTO_LOCK_TIMER')}
                         iconLeading='clock'
                         rightContent={
                             <p className='typeset-headline mr-2 text-base font-normal text-theme-secondary-500 dark:text-theme-secondary-300'>
@@ -173,7 +175,7 @@ export const SettingsMenu = ({
                         }
                     />
                     <SettingsOption
-                        title='About ARK Connect'
+                        title={t('PAGES.SETTINGS.MENU.ABOUT_ARK_CONNECT')}
                         iconLeading='support'
                         iconTrailing='arrow-right'
                         onClick={() => handleNavigation('/about')}
@@ -182,7 +184,7 @@ export const SettingsMenu = ({
                         }
                     />
                     <SettingsOption
-                        title='Remove Addresses'
+                        title={t('PAGES.SETTINGS.MENU.REMOVE_ADDRESSES')}
                         iconLeading='trash'
                         iconTrailing='arrow-right'
                         onClick={handleRemoveAddressClick}
