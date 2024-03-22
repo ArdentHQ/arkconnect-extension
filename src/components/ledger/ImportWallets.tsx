0ccf2ccf57173f13d6d0cf43253b4c39acdfc624
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-03-22 09:21:16 -0500

    fix: bg issues

--- src/components/ledger/ImportWallets.tsx
+++ src/components/ledger/ImportWallets.tsx
@@ -135,12 +135,13 @@ const ImportWallets = ({ goToNextStep, formik }: Props) => {
                         return (
                             <div
                                 className={cn(
-                                    'flex cursor-pointer justify-between transition-all duration-500 ease-in-out hover:bg-theme-secondary-50 dark:bg-theme-secondary-700',
+                                    'flex cursor-pointer justify-between transition-all duration-500 ease-in-out hover:bg-theme-secondary-50',
                                     {
                                         'bg-theme-secondary-100 text-theme-secondary-500 dark:bg-light-black dark:text-theme-secondary-300':
                                             isImported,
-                                        'text-light-black dark:bg-subtle-black dark:text-white dark:hover:bg-theme-secondary-700':
+                                        'text-light-black dark:text-white':
                                             !isImported,
+                                        'dark:bg-subtle-black dark:hover:bg-theme-secondary-700': !isImported && !isSelected(wallet.path),
                                         'bg-theme-primary-50 hover:bg-theme-primary-50 dark:bg-theme-primary-650/15 dark:hover:bg-theme-primary-650/15':
                                             !isImported && isSelected(wallet.path),
                                     },
