9798bd25e924c4f38b0b84f0e5115020c0706bd4
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-03-25 12:15:50 +0000

    style: resolve style guide violations

--- src/components/approve/ActionDetailsValue.tsx
+++ src/components/approve/ActionDetailsValue.tsx
@@ -2,12 +2,12 @@ import { forwardRef } from 'react';
 
 export const ActionDetailsValue = forwardRef<HTMLDivElement, { children: React.ReactNode }>(
     ({ children }, ref) => {
-      return (
-        <div ref={ref} className='text-sm font-medium text-light-black dark:text-white'>
-          {children}
-        </div>
-      );
-    }
+        return (
+            <div ref={ref} className='text-sm font-medium text-light-black dark:text-white'>
+                {children}
+            </div>
+        );
+    },
 );
 
 ActionDetailsValue.displayName = 'ActionDetailsValue';
