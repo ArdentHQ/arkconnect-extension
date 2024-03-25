4a85e681af346234555f72547e4d8bb1300fe669
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-03-25 06:32:21 -0500

    refactor: action details value

---
+++ src/components/approve/ActionDetailsValue.tsx
@@ -0,0 +1,13 @@
+import { forwardRef } from 'react';
+
+export const ActionDetailsValue = forwardRef<HTMLDivElement, { children: React.ReactNode }>(
+    ({ children }, ref) => {
+      return (
+        <div ref={ref} className='text-sm font-medium text-light-black dark:text-white'>
+          {children}
+        </div>
+      );
+    }
+);
+
+ActionDetailsValue.displayName = 'ActionDetailsValue';
