ab84987d4ac3a50fa04d77842bd1299aa49f0724
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-04-02 11:46:19 +0000

    style: resolve style guide violations

--- src/lib/utils/convertHexToRgba.test.ts
+++ src/lib/utils/convertHexToRgba.test.ts
@@ -7,21 +7,21 @@ describe('convertHexToRgba', () => {
         const opacity = '1';
         const expected = 'rgba(255, 0, 0, 1)';
         expect(convertHexToRGBA(hex, opacity)).toEqual(expected);
-      });
-    
-      it('should convert hex to RGBA with opacity 0.5', () => {
+    });
+
+    it('should convert hex to RGBA with opacity 0.5', () => {
         const hex = '#00FF00';
         const opacity = '0.5';
         const expected = 'rgba(0, 255, 0, 0.5)';
-        
+
         expect(convertHexToRGBA(hex, opacity)).toEqual(expected);
-      });
-    
-      it('should convert hex to RGBA with opacity 0', () => {
+    });
+
+    it('should convert hex to RGBA with opacity 0', () => {
         const hex = '#0000FF';
         const opacity = '0';
         const expected = 'rgba(0, 0, 255, 0)';
-    
+
         expect(convertHexToRGBA(hex, opacity)).toEqual(expected);
-      });
+    });
 });
