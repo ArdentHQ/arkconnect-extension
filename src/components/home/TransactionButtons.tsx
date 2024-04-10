9657f28d4166c85d4e604007300d52b4edb5bbec
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-04-10 14:08:06 +0000

    style: resolve style guide violations

--- src/components/home/TransactionButtons.tsx
+++ src/components/home/TransactionButtons.tsx
@@ -3,31 +3,26 @@ import { usePrimaryWallet } from '@/lib/hooks/usePrimaryWallet';
 import { Button, Tooltip } from '@/shared/components';
 
 export const TransactionButtons = () => {
-  const { t } = useTranslation();
-  const primaryWallet = usePrimaryWallet();
-  const walletBalance = primaryWallet?.balance() ?? 0;
+    const { t } = useTranslation();
+    const primaryWallet = usePrimaryWallet();
+    const walletBalance = primaryWallet?.balance() ?? 0;
 
-  return (
-    <div className='grid grid-cols-2 gap-2 justify-center items-center mx-4'>
-
-    <Tooltip
-        disabled={walletBalance > 0}
-        content={
-            <p>
-                {t('PAGES.HOME.INSUFFICIENT_FUNDS')}
-            </p>
-        }
-        placement='top'
-    >
-        <div>
-            <Button iconLeading='send' variant='secondary' disabled={walletBalance === 0}>
-                {t('COMMON.SEND')}
+    return (
+        <div className='mx-4 grid grid-cols-2 items-center justify-center gap-2'>
+            <Tooltip
+                disabled={walletBalance > 0}
+                content={<p>{t('PAGES.HOME.INSUFFICIENT_FUNDS')}</p>}
+                placement='top'
+            >
+                <div>
+                    <Button iconLeading='send' variant='secondary' disabled={walletBalance === 0}>
+                        {t('COMMON.SEND')}
+                    </Button>
+                </div>
+            </Tooltip>
+            <Button iconLeading='receive' variant='secondary'>
+                {t('COMMON.RECEIVE')}
             </Button>
         </div>
-    </Tooltip>
-        <Button iconLeading='receive' variant='secondary'>
-            {t('COMMON.RECEIVE')}
-        </Button>
-    </div>
-  );
+    );
 };
