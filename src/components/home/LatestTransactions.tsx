9f07d48bf0ad0adf8810b3f39b6d1c0456bb381a
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-04-10 19:38:42 +0000

    style: resolve style guide violations

--- src/components/home/LatestTransactions.tsx
+++ src/components/home/LatestTransactions.tsx
@@ -2,30 +2,30 @@ import { useTranslation } from 'react-i18next';
 import { EmptyConnectionsIcon } from '@/shared/components';
 
 const NoTransactions = () => {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  return (
-    <div className="mt-12 flex flex-col items-center justify-center gap-6">
-      <EmptyConnectionsIcon />
-      <div className='max-w-40 leading-tight text-base font-normal text-center dark:text-white'>
-        {t('PAGES.HOME.NO_TRANSACTIONS')}
-      </div>
-    </div>
-  );
+    return (
+        <div className='mt-12 flex flex-col items-center justify-center gap-6'>
+            <EmptyConnectionsIcon />
+            <div className='max-w-40 text-center text-base font-normal leading-tight dark:text-white'>
+                {t('PAGES.HOME.NO_TRANSACTIONS')}
+            </div>
+        </div>
+    );
 };
 
 export const LatestTransactions = () => {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  return (
-    <div className="w-full h-full bg-white rounded-t-2xl dark:bg-subtle-black mt-4">
-      <div className="border-b border-b-theme-secondary-200 p-4 text-light-black dark:text-white dark:border-b-theme-secondary-600 leading-tight font-medium text-lg">
-        {t('PAGES.HOME.LATEST_TRANSACTIONS')}
-      </div>
+    return (
+        <div className='mt-4 h-full w-full rounded-t-2xl bg-white dark:bg-subtle-black'>
+            <div className='border-b border-b-theme-secondary-200 p-4 text-lg font-medium leading-tight text-light-black dark:border-b-theme-secondary-600 dark:text-white'>
+                {t('PAGES.HOME.LATEST_TRANSACTIONS')}
+            </div>
 
-      <div className="w-full h-auto">
-        <NoTransactions />
-      </div>
-    </div>
-  );
+            <div className='h-auto w-full'>
+                <NoTransactions />
+            </div>
+        </div>
+    );
 };
