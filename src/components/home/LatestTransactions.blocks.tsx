6e691c556cf2417a510c859079a3f15838e33b37
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-04-12 14:56:21 -0500

    fix: icon size

--- src/components/home/LatestTransactions.blocks.tsx
+++ src/components/home/LatestTransactions.blocks.tsx
@@ -145,11 +145,11 @@ const TransactionListItem = ({ transaction }: { transaction: ConfirmedTransactio
     const formattedTimestamp = dayjs(timestamp).format('DD MMM YYYY HH:mm:ss');
 
     const isSpecialTransaction = [
-        TransactionType.RETURN,
         TransactionType.REGISTRATION,
         TransactionType.RESIGNATION,
         TransactionType.OTHER,
         TransactionType.SECOND_SIGNATURE,
+        TransactionType.MULTISIGNATURE
     ].includes(type as TransactionType);
 
     return (
@@ -157,8 +157,9 @@ const TransactionListItem = ({ transaction }: { transaction: ConfirmedTransactio
             <div className='flex h-11 min-w-11 items-center justify-center rounded-xl border border-theme-secondary-200 bg-white text-theme-secondary-500 dark:border-theme-secondary-600 dark:bg-subtle-black dark:text-theme-secondary-300'>
                 <Icon
                     className={cn({
-                        'h-[22px] w-[22px]': isSpecialTransaction,
-                        'h-8 w-8': !isSpecialTransaction,
+                        'h-5 w-5': isSpecialTransaction,
+                        'h-8 w-8': !isSpecialTransaction && type !== TransactionType.RETURN,
+                        'h-[22px] w-[22px]': !isSpecialTransaction && type === TransactionType.RETURN,
                     })}
                     icon={type as IconDefinition}
                 />
