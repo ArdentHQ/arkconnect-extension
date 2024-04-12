cf9c9ea2f33b2d9e02d739a2256c9ca06675c5ce
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-04-11 23:03:00 -0500

    feat: implement caching for latest transactions list

--- src/components/home/LatestTransactions.tsx
+++ src/components/home/LatestTransactions.tsx
@@ -1,35 +1,43 @@
 import { useTranslation } from 'react-i18next';
 import { ConfirmedTransactionData } from '@ardenthq/sdk/distribution/esm/confirmed-transaction.dto.contract';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { IReadWriteWallet } from '@ardenthq/sdk-profiles/distribution/esm/wallet.contract';
+import { useQuery } from 'react-query';
 import { NoTransactions, TransactionsList } from './LatestTransactions.blocks';
 import { usePrimaryWallet } from '@/lib/hooks/usePrimaryWallet';
+
+const fetchTransactions = async (primaryWallet?: IReadWriteWallet) => {
+    try {
+        const response = await primaryWallet?.client().transactions({ 
+            limit: 10, 
+            identifiers: [{ type: 'address', value: primaryWallet?.address() }] 
+        });
+        return response?.items() || [];
+    } catch (error) {
+        return [];
+    }
+};
 
 export const LatestTransactions = () => {
   const { t } = useTranslation();
   const primaryWallet = usePrimaryWallet();
 
-  const [transactions, setTransactions] = useState<ConfirmedTransactionData[]>([]);
-
-  const fetchTransactions = async () => {
-      try {
-          const response = await primaryWallet?.client().transactions({ 
-              limit: 10, 
-              identifiers: [{ type: 'address', value: primaryWallet?.address() }] 
-          });
-          return response?.items() || [];
-      } catch (error) {
-          return [];
-      }
-  };
+  const { data: transactions = [], refetch } = useQuery<ConfirmedTransactionData[]>(
+    ['transactions', primaryWallet?.address()],
+    () => fetchTransactions(primaryWallet),
+    { 
+      enabled: !!primaryWallet, 
+      staleTime: 0,
+      initialData: [],
+      refetchInterval: 3000 
+    }
+);
 
   useEffect(() => {
-      const fetchAndSetData = async () => {
-          const transactions = await fetchTransactions();
-          setTransactions(transactions);
-      };
-
-      fetchAndSetData();
-  }, []);
+    if (primaryWallet) {
+      refetch();
+    }
+  }, [primaryWallet, refetch]);
 
   return (
     <div className="w-full h-full bg-white rounded-t-2xl dark:bg-subtle-black mt-4">
