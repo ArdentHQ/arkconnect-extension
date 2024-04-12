297e46b9efa4c671429cef17ffba79ab5776c14c
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-04-11 22:02:43 -0500

    feat: implement latest transactions list

--- src/components/home/LatestTransactions.tsx
+++ src/components/home/LatestTransactions.tsx
@@ -1,21 +1,35 @@
 import { useTranslation } from 'react-i18next';
-import { EmptyConnectionsIcon } from '@/shared/components';
-
-const NoTransactions = () => {
-  const { t } = useTranslation();
-
-  return (
-    <div className="mt-12 flex flex-col items-center justify-center gap-6">
-      <EmptyConnectionsIcon />
-      <div className='max-w-40 leading-tight text-base font-normal text-center dark:text-white'>
-        {t('PAGES.HOME.NO_TRANSACTIONS')}
-      </div>
-    </div>
-  );
-};
+import { ConfirmedTransactionData } from '@ardenthq/sdk/distribution/esm/confirmed-transaction.dto.contract';
+import { useEffect, useState } from 'react';
+import { NoTransactions, TransactionsList } from './LatestTransactions.blocks';
+import { usePrimaryWallet } from '@/lib/hooks/usePrimaryWallet';
 
 export const LatestTransactions = () => {
   const { t } = useTranslation();
+  const primaryWallet = usePrimaryWallet();
+
+  const [transactions, setTransactions] = useState<ConfirmedTransactionData[]>([]);
+
+  const fetchTransactions = async () => {
+      try {
+          const response = await primaryWallet?.client().transactions({ 
+              limit: 10, 
+              identifiers: [{ type: 'address', value: primaryWallet?.address() }] 
+          });
+          return response?.items() || [];
+      } catch (error) {
+          return [];
+      }
+  };
+
+  useEffect(() => {
+      const fetchAndSetData = async () => {
+          const transactions = await fetchTransactions();
+          setTransactions(transactions);
+      };
+
+      fetchAndSetData();
+  }, []);
 
   return (
     <div className="w-full h-full bg-white rounded-t-2xl dark:bg-subtle-black mt-4">
@@ -24,7 +38,11 @@ export const LatestTransactions = () => {
       </div>
 
       <div className="w-full h-auto">
-        <NoTransactions />
+        {
+          transactions.length > 0 ? <TransactionsList transactions={transactions} />:
+          <NoTransactions />
+        }
+        
       </div>
     </div>
   );
