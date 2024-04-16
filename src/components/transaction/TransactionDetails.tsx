6b12a371b18edd822aa846185e790547bf025fdf
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-04-16 18:15:48 -0500

    feat: initial transaction details page

---
+++ src/components/transaction/TransactionDetails.tsx
@@ -0,0 +1,13 @@
+import { useTranslation } from 'react-i18next';
+import SubPageLayout from '@/components/settings/SubPageLayout';
+
+
+export const TransactionDetails = () => {
+  const { t } = useTranslation();
+
+  return (
+    <SubPageLayout title={t('PAGES.TRANSACTION_DETAILS.PAGE_TITLE')}>
+      Content
+    </SubPageLayout>
+  );
+};
