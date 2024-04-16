0f6cfdd237dc476f322bc7dcf96159940eaf8fad
Patricio Marroquin <55117912+patricio0312rev@users.noreply.github.com>
2024-04-16 03:37:03 -0500

    feat: handle additional types for transactions (#219)

--- src/components/home/LatestTransactions.blocks.tsx
+++ src/components/home/LatestTransactions.blocks.tsx
@@ -64,6 +64,18 @@ const TransactionListItem = ({ transaction }: { transaction: ConfirmedTransactio
         if (transaction.isUnvote()) {
             return TransactionType.UNVOTE;
         }
+        if (transaction.isSecondSignature()) {
+            return TransactionType.SECOND_SIGNATURE;
+        }
+        if (transaction.isMultiSignatureRegistration()) {
+            return TransactionType.MULTISIGNATURE;
+        }
+        if (transaction.isDelegateRegistration()) {
+            return TransactionType.REGISTRATION;
+        }
+        if (transaction.isDelegateResignation()) {
+            return TransactionType.RESIGNATION;
+        }
         return TransactionType.OTHER;
     };
 
@@ -83,6 +95,14 @@ const TransactionListItem = ({ transaction }: { transaction: ConfirmedTransactio
                 return t('COMMON.VOTE');
             case TransactionType.UNVOTE:
                 return t('COMMON.UNVOTE');
+            case TransactionType.SECOND_SIGNATURE:
+                return t('COMMON.SECOND_SIGNATURE');
+            case TransactionType.REGISTRATION:
+                return t('COMMON.REGISTRATION');
+            case TransactionType.RESIGNATION:
+                return t('COMMON.RESIGNATION');
+            case TransactionType.MULTISIGNATURE:
+                return t('COMMON.MULTISIGNATURE');
             default:
                 return t('COMMON.OTHER');
         }
@@ -124,13 +144,23 @@ const TransactionListItem = ({ transaction }: { transaction: ConfirmedTransactio
     const timestamp = transaction.timestamp()?.toString() ?? '';
     const formattedTimestamp = dayjs(timestamp).format('DD MMM YYYY HH:mm:ss');
 
+    const isSpecialTransaction = [
+        TransactionType.REGISTRATION,
+        TransactionType.RESIGNATION,
+        TransactionType.OTHER,
+        TransactionType.SECOND_SIGNATURE,
+        TransactionType.MULTISIGNATURE,
+    ].includes(type as TransactionType);
+
     return (
         <div className='transition-smoothEase flex h-[76px] w-full flex-row items-center justify-center gap-3 p-4 hover:bg-theme-secondary-50 dark:hover:bg-theme-secondary-700'>
             <div className='flex h-11 min-w-11 items-center justify-center rounded-xl border border-theme-secondary-200 bg-white text-theme-secondary-500 dark:border-theme-secondary-600 dark:bg-subtle-black dark:text-theme-secondary-300'>
                 <Icon
                     className={cn({
-                        'h-[22px] w-[22px]': type === TransactionType.RETURN,
-                        'h-8 w-8': type !== TransactionType.RETURN,
+                        'h-5 w-5': isSpecialTransaction,
+                        'h-8 w-8': !isSpecialTransaction && type !== TransactionType.RETURN,
+                        'h-[22px] w-[22px]':
+                            !isSpecialTransaction && type === TransactionType.RETURN,
                     })}
                     icon={type as IconDefinition}
                 />
