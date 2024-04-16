f608e3b640a61af2203b2216fc050f1f728e17d3
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-04-16 23:34:42 +0000

    style: resolve style guide violations

--- src/pages/TransactionDetails.tsx
+++ src/pages/TransactionDetails.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from 'react-i18next';
 import SubPageLayout from '@/components/settings/SubPageLayout';
 
 const TransactionDetails = () => {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
     return <SubPageLayout title={t('PAGES.TRANSACTION_DETAILS.PAGE_TITLE')}>Content</SubPageLayout>;
 };
