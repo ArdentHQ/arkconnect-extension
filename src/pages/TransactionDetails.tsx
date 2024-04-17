5106c7c434c71498d7600901a9d3c4ffca05f178
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-04-16 19:58:05 -0500

    feat: fetch transaction details data

--- src/pages/TransactionDetails.tsx
+++ src/pages/TransactionDetails.tsx
@@ -1,10 +1,49 @@
 import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { ExtendedConfirmedTransactionData } from '@ardenthq/sdk-profiles/distribution/esm/transaction.dto';
+import { Loader } from '../shared/components/loader/Loader';
+import { usePrimaryWallet } from '@/lib/hooks/usePrimaryWallet';
 import SubPageLayout from '@/components/settings/SubPageLayout';
+import { TransactionHeader } from '@/components/transaction/details/TransactionHeader';
+import { getType, TransactionType } from '@/components/home/LatestTransactions.blocks';
 
 const TransactionDetails = () => {
     const { t } = useTranslation();
+    const primaryWallet = usePrimaryWallet();
+    const { transactionId } = useParams<{ transactionId: string }>();
 
-    return <SubPageLayout title={t('PAGES.TRANSACTION_DETAILS.PAGE_TITLE')}>Content</SubPageLayout>;
+    const [transactionData, setTransactionData] = useState<ExtendedConfirmedTransactionData>();
+    
+    const fetchTransactionData = async () => {
+        try {
+            const response = await primaryWallet?.transactionIndex().findById(transactionId ?? '');
+            return response;
+        } catch (error) {
+            return [];
+        }
+    };
+
+
+    useEffect(() => {
+        const fetchAndSetData = async () => {
+            const data = await fetchTransactionData();
+
+            if (data && !Array.isArray(data)) {
+                setTransactionData(data);
+            }
+        };
+
+        fetchAndSetData();
+    }, []);
+
+    return (
+        <SubPageLayout title={t('PAGES.TRANSACTION_DETAILS.PAGE_TITLE')}>
+            {
+                transactionData ? <TransactionHeader type={getType(transactionData) as TransactionType} /> : <Loader/>
+            }
+        </SubPageLayout>
+    );
 };
 
 export default TransactionDetails;
