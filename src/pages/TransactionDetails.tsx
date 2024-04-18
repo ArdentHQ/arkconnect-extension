f4cab265dd7473252053e6e4616a6acc13f21837
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-04-18 00:50:37 -0500

    chore: implement transaction body component

--- src/pages/TransactionDetails.tsx
+++ src/pages/TransactionDetails.tsx
@@ -1,10 +1,15 @@
 import { useTranslation } from 'react-i18next';
 import SubPageLayout from '@/components/settings/SubPageLayout';
+import { TransactionBody } from '@/components/transaction/details/TransactionBody';
 
 const TransactionDetails = () => {
     const { t } = useTranslation();
 
-    return <SubPageLayout title={t('PAGES.TRANSACTION_DETAILS.PAGE_TITLE')}>Content</SubPageLayout>;
+    return (
+        <SubPageLayout title={t('PAGES.TRANSACTION_DETAILS.PAGE_TITLE')}>
+            <TransactionBody/>
+        </SubPageLayout>
+    );
 };
 
 export default TransactionDetails;
