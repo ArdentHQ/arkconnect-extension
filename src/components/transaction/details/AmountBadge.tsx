21ce2fc54957bf5f90f92afbe599a0e3130ffd70
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-04-18 00:49:57 -0500

    feat: amount badge component

---
+++ src/components/transaction/details/AmountBadge.tsx
@@ -0,0 +1,17 @@
+import cn from 'classnames';
+
+export const AmountBadge = ({
+    amount,
+    type = 'default'
+}: {
+    amount: string,
+    type?: 'positive' | 'negative' | 'default'
+}) => {
+  return (
+    <div className={cn('p-1.5 flex items-center justify-center rounded-md border', {
+        'border-theme-secondary-500 text-light-black dark:border-theme-secondary-300 dark:text-white': type === 'default',
+        'border-theme-primary-700 text-theme-primary-700 dark:border-theme-primary-600 dark:text-theme-primary-600' : type === 'positive',
+        'border-theme-error-600 text-theme-error-600 dark:border-theme-error-500 dark:text-theme-error-500': type === 'negative',
+    })}>{amount}</div>
+  );
+};
