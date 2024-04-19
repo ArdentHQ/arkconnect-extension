b4b6023f5b0d811902786be9f521bf29f2506f07
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-04-19 08:38:00 -0500

    refactor: transaction header component

--- src/components/transaction/details/TransactionHeader.tsx
+++ src/components/transaction/details/TransactionHeader.tsx
@@ -1,10 +1,14 @@
-import { getTitle, TransactionType } from '@/components/home/LatestTransactions.utils';
+import { twMerge } from 'tailwind-merge';
+import { ExtendedConfirmedTransactionData } from '@ardenthq/sdk-profiles/distribution/esm/transaction.dto';
+import { getTitle, getType, TransactionType } from '@/components/home/LatestTransactions.utils';
 import { TransactionIcon } from '@/components/transaction/Transaction.blocks';
 
-export const TransactionHeader = ({ type }: { type: TransactionType }) => {
+export const TransactionHeader = ({ transaction, className }: { transaction: ExtendedConfirmedTransactionData, className?: string }) => {
+    const type= getType(transaction);
+
     return (
-        <div className='flex flex-row items-center gap-3'>
-            <TransactionIcon type={type} />
+        <div className={twMerge('flex flex-row items-center gap-3', className)}>
+            <TransactionIcon type={type as TransactionType} />
             <h4 className='text-base font-medium leading-5 text-light-black dark:text-white'>
                 {getTitle(type)}
             </h4>
