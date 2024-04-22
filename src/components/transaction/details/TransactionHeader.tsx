94bc2f18836699ce13eca1258dc45ff1e49421d4
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-04-22 13:19:24 -0500

    refactor: support for multipayments in transaction details header

--- src/components/transaction/details/TransactionHeader.tsx
+++ src/components/transaction/details/TransactionHeader.tsx
@@ -2,6 +2,7 @@ import { twMerge } from 'tailwind-merge';
 import { ExtendedConfirmedTransactionData } from '@ardenthq/sdk-profiles/distribution/esm/transaction.dto';
 import { getTitle, getType, TransactionType } from '@/components/home/LatestTransactions.utils';
 import { TransactionIcon } from '@/components/transaction/Transaction.blocks';
+import { MultipaymentBadge } from '@/components/home/LatestTransactions.blocks';
 
 export const TransactionHeader = ({
     transaction,
@@ -10,13 +11,18 @@ export const TransactionHeader = ({
     transaction: ExtendedConfirmedTransactionData;
     className?: string;
 }) => {
+    let subtype;
     const type = getType(transaction);
+
+    if(type === TransactionType.MULTIPAYMENT) {
+        subtype = transaction.isSent() ? TransactionType.SEND : TransactionType.RECEIVE;
+    }
 
     return (
         <div className={twMerge('flex flex-row items-center gap-3', className)}>
-            <TransactionIcon type={type as TransactionType} />
+            <TransactionIcon type={subtype || type as TransactionType} />
             <h4 className='text-base font-medium leading-5 text-light-black dark:text-white'>
-                {getTitle(type)}
+                {getTitle(type, transaction.isSent())} {type === TransactionType.MULTIPAYMENT && <MultipaymentBadge />}
             </h4>
         </div>
     );
