6e508dcd238c9592b4deeeb7e5bff3614ddc2e52
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-04-23 06:37:36 -0500

    refactor: support for custom class names in action detail component

--- src/components/approve/ActionDetailsValue.tsx
+++ src/components/approve/ActionDetailsValue.tsx
@@ -1,9 +1,10 @@
 import { forwardRef } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-export const ActionDetailsValue = forwardRef<HTMLDivElement, { children: React.ReactNode }>(
-    ({ children }, ref) => {
+export const ActionDetailsValue = forwardRef<HTMLDivElement, { children: React.ReactNode, className?: string }>(
+    ({ children, className }, ref) => {
         return (
-            <div ref={ref} className='text-sm font-medium text-light-black dark:text-white'>
+            <div ref={ref} className={twMerge('text-sm font-medium text-light-black dark:text-white', className)}>
                 {children}
             </div>
         );
