0a45e7156e297cbba6bc19e7924f3a9a2fd7b679
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-04-25 08:22:47 -0500

    test: formatUnixTimestamp

---
+++ src/lib/utils/formatUnixTimestsamp.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest';
+import { formatUnixTimestamp } from './formatUnixTimestsamp';
+
+describe('formatUnixTimestsamp', () => {
+    it('should correctly format a Unix timestamp at the start of an epoch', () => {
+        const timestamp = 0;
+        const result = formatUnixTimestamp(timestamp);
+        expect(result).toBe('01 Jan 1970, 00:00:00');
+    });
+
+    it('should correctly format a Unix timestamp at a specific date', () => {
+        const timestamp = 1672444800;
+        const result = formatUnixTimestamp(timestamp);
+        expect(result).toBe('01 Jan 2023, 00:00:00');
+    });
+});
