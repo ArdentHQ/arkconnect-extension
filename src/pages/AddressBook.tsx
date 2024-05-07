375eff33863a1034b7e6831fa9af1da749711ca3
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-07 14:53:18 -0500

    refactor: implement address book list if wallet has addresses stored

--- src/pages/AddressBook.tsx
+++ src/pages/AddressBook.tsx
@@ -1,14 +1,27 @@
 import { useTranslation } from 'react-i18next';
 import SubPageLayout from '@/components/settings/SubPageLayout';
-import { AddContactButton, NoContacts } from '@/components/address-book';
+import { AddContactButton, AddressBookList, NoContacts } from '@/components/address-book';
+import useAddressBook from '@/lib/hooks/useAddressBook';
+import { usePrimaryWallet } from '@/lib/hooks/usePrimaryWallet';
 
 const AddressBook = () => {
+    const primaryWallet = usePrimaryWallet();
     const { t } = useTranslation();
+    const { getAddressBook } = useAddressBook();
+    const currentAddressBook = getAddressBook(primaryWallet?.address() || '');
 
     return (
-        <SubPageLayout title={t('PAGES.ADDRESS_BOOK.TITLE')} className='relative'>
-            <NoContacts className='mt-[114px]' />
-            <div className='absolute -bottom-4 left-0 w-full'>
+        <SubPageLayout title={t('PAGES.ADDRESS_BOOK.TITLE')} className='relative p-0'>
+            {
+                currentAddressBook.length > 0 ? (
+                    <div className='mx-4 min-h-[calc(100%-68px)]'>
+                        <AddressBookList />
+                    </div>
+                ) : (
+                    <NoContacts className='min-h-[calc(100%-68px)]' />
+                )
+            }
+            <div className='sticky bottom-0 left-0 w-full'>
                 <AddContactButton />
             </div>
         </SubPageLayout>
