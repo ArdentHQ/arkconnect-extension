7f163421e6f647defcd5dc5235fe8d6f4a65b305
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-07 10:37:13 -0500

    chore: update translation files

--- src/i18n/ns/error.ts
+++ src/i18n/ns/error.ts
@@ -1,6 +1,9 @@
 export default {
-    AMOUNT_INVALID: 'Amount is invalid',
-    AMOUNT_TOO_HIGH: 'Amount is too high',
+    IS_DUPLICATED: 'This {{name}} already exists',
+    IS_INVALID: 'Invalid {{name}}',
+    IS_TOO_HIGH: '{{name}} is too high',
+    IS_TOO_LONG: '{{name}} is too long',
+    IS_REQUIRED: '{{name}} is required',
+    MAX_CHARACTERS: '{{count}} characters maximum',
     LEDGER_TRANSPORT_NOT_SUPPORTED: 'Ledger Transport is not supported!',
-    MEMO_TOO_LONG: 'Memo exceeds limit',
 };
