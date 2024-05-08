8e7dab1e6ad6127fa876c2c8cdf55cfc2a380a3c
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-07 21:39:46 -0500

    feat: remove address modal body component

---
+++ src/components/address-book/remove/RemoveAddress.tsx
@@ -0,0 +1,23 @@
+import { useTranslation } from 'react-i18next';
+import { Heading, HeadingDescription } from '@/shared/components';
+
+
+export const RemoveAddress = ({
+    name,
+}: {
+    name: string;
+}) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className='flex flex-col gap-2'>
+            <Heading level={4}>
+                {t('PAGES.ADDRESS_BOOK.DELETE_SAVED_CONTACT')}?
+            </Heading>
+
+            <HeadingDescription>
+                {t('PAGES.ADDRESS_BOOK.ARE_YOU_SURE_YOU_WANT_TO_DELETE')}{' '}<span className='text-light-black dark:text-white'>{name}</span>{' '}{t('PAGES.ADDRESS_BOOK.FROM_YOUR_ADDRESS_BOOK')}?
+            </HeadingDescription>
+        </div>
+    );
+};
