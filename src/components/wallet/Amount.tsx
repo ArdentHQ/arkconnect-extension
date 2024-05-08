521702a17fa6266d989f22738cb2dfe3187d6b4c
ItsANameToo <itsanametoo@protonmail.com>
2024-05-08 12:16:26 +0200

    fix: improve small amount styling

--- src/components/wallet/Amount.tsx
+++ src/components/wallet/Amount.tsx
@@ -1,8 +1,8 @@
-import { Helpers } from '@ardenthq/sdk-profiles';
-import { TippyProps } from '@tippyjs/react';
 import cn from 'classnames';
 import constants from '@/constants';
 import cropToMaxDigits from '@/lib/utils/cropToMaxDigits';
+import { Helpers } from '@ardenthq/sdk-profiles';
+import { TippyProps } from '@tippyjs/react';
 import { Tooltip } from '@/shared/components';
 
 interface AmountProperties {
@@ -32,6 +32,7 @@ const Amount = ({
     displayTooltip = true,
     hideSmallValues = false,
 }: AmountProperties) => {
+    value = value / 100000;
     let actualFormattedAmount = Helpers.Currency.format(value, ticker, { withTicker });
     const valueToFormat = hideSmallValues && value !== 0 && value < 0.01 ? 0.01 : value;
 
@@ -44,7 +45,7 @@ const Amount = ({
     });
 
     if (valueToFormat !== value) {
-        formattedAmount = `< ${formattedAmount}`;
+        formattedAmount = ` <${formattedAmount}`; // Note: has a space before it to avoid "+<0.01"
     }
 
     if (value === 0 && !['ARK', 'DARK'].includes(formattedAmount.split(' ')[1])) {
