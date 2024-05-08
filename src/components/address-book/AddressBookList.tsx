b1952cdbef8fcd902f27877c229a0796d575daec
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-07 21:39:30 -0500

    refactor: update props for address book list

--- src/components/address-book/AddressBookList.tsx
+++ src/components/address-book/AddressBookList.tsx
@@ -1,11 +1,10 @@
 import { TestnetIcon } from '@/components/wallet/address/Address.blocks';
-import useAddressBook from '@/lib/hooks/useAddressBook';
+import { Contact } from '@/lib/hooks/useAddressBook';
 import { WalletNetwork } from '@/lib/store/wallet';
 import trimAddress from '@/lib/utils/trimAddress';
 import { IconButton, Tooltip } from '@/shared/components';
 
-const AddressBookItem = ({ name, address, network }: { name: string; address: string, network: WalletNetwork }) => {
-
+const AddressBookItem = ({ name, address, network, handleRemoveContact }: { name: string; address: string, network: WalletNetwork, handleRemoveContact: (name: string) => void }) => {
     return (
         <div className='transition-smoothEase flex w-full flex-row items-center justify-between px-4 py-3 hover:bg-theme-secondary-50 dark:hover:bg-theme-secondary-700'>
             <div className='flex flex-col gap-1 '>
@@ -24,19 +23,23 @@ const AddressBookItem = ({ name, address, network }: { name: string; address: st
 
             <div className='flex h-full flex-row items-end justify-center gap-0.5'>
                 <IconButton icon='pencil' />
-                <IconButton icon='trash' variant='danger' />
+                <IconButton onClick={() => handleRemoveContact(name)} icon='trash' variant='danger' />
             </div>
         </div>
     );
 };
 
-export const AddressBookList = () => {
-    const { addressBook } = useAddressBook();
-
+export const AddressBookList = ({
+    handleRemoveContact,
+    addressBook
+}: {
+    handleRemoveContact: (name: string) => void;
+    addressBook: Contact[];
+}) => {
     return (
         <div className='flex w-full flex-col overflow-hidden rounded-2xl bg-white py-2 shadow-address-book dark:bg-subtle-black'>
             {addressBook.map((contact, index) => (
-                <AddressBookItem key={index} name={contact.name} address={contact.address} network={contact.type} />
+                <AddressBookItem key={index} name={contact.name} address={contact.address} network={contact.type} handleRemoveContact={handleRemoveContact} />
             ))}
         </div>
     );
