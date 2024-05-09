5b25d1b2cf9ebaafe20dc02b3e199d8317620298
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-08 21:15:49 -0500

    feat: edit contact form

---
+++ src/components/address-book/edit/EditContactForm.tsx
@@ -0,0 +1,41 @@
+import { useTranslation } from 'react-i18next';
+import { FormikProps } from 'formik';
+import { AddContactFormik } from '@/pages/CreateContact';
+import { Input, TextArea } from '@/shared/components';
+
+export const EditContactForm = ({
+    formik
+}: {
+    formik: FormikProps<AddContactFormik>
+}) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className='flex flex-col gap-4'>
+            <Input
+                type='text'
+                labelText={t('COMMON.NAME')}
+                placeholder={t('COMMON.ENTER_NAME')}
+                name='name'
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                variant={formik.errors.name ? 'destructive' : 'primary'}
+                helperText={formik.errors.name}
+            />
+            <TextArea
+                labelText={t('COMMON.ADDRESS')}
+                placeholder={t('COMMON.ENTER_ADDRESS')}
+                rows={2}
+                className='h-[72px]'
+                name='address'
+                value={formik.values.address}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                variant={formik.errors.address ? 'destructive' : 'primary'}
+                helperText={formik.errors.address}
+            />
+        </div>
+    );
+};
+
