0eccd070108c09edafaddc10ec0a84dac42b6fbe
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-10 10:46:45 -0500

    feat: send form

---
+++ src/components/send/SendForm.tsx
@@ -0,0 +1,71 @@
+import { useTranslation } from 'react-i18next';
+import { FormikProps } from 'formik';
+import Amount from '@/components/wallet/Amount';
+import { Input } from '@/shared/components';
+import { usePrimaryWallet } from '@/lib/hooks/usePrimaryWallet';
+import { SendFormik } from '@/pages/Send';
+
+export const SendForm = ({
+    formik
+}: {
+    formik: FormikProps<SendFormik>
+}) => {
+    const primaryWallet = usePrimaryWallet();
+    const { t } = useTranslation();
+
+    const handleMaxClick = () => {
+        formik.setFieldValue('amount', primaryWallet?.balance().toString() || '0');
+    };
+
+    const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.target.value = event.target.value.trim();
+        formik.handleChange(event);
+    };
+
+    return (
+        <div className='flex flex-col gap-4'>
+            <Input 
+                name='amount'
+                labelText={t('COMMON.AMOUNT')}
+                secondaryText={<span>
+                {`${t('COMMON.AVAILABLE')}:`}
+                <Amount
+                    value={primaryWallet?.balance() || 0}
+                    ticker={primaryWallet?.currency() || 'ARK'}
+                    withTicker
+                    showSign={false}
+                    isNegative={false}
+                    maxDigits={20}
+                    displayTooltip={false}
+                    maxDecimals={2}
+                    hideSmallValues
+                /> 
+                </span>
+                }
+                placeholder={t('COMMON.ENTER_AMOUNT')}
+                trailing={
+                    <button onClick={handleMaxClick} className='uppercase p-1 rounded text-theme-primary-700 hover:text-theme-primary-600 hover:bg-theme-secondary-50 dark:text-theme-primary-600     dark:hover:bg-theme-secondary-700 dark:shadow-secondary-dark dark:hover:text-theme-primary-650 transition-smoothEase'>
+                        {t('COMMON.MAX')}
+                    </button>
+                }
+                value={formik.values.amount}
+                onChange={handleAmountChange}
+                onBlur={formik.handleBlur}
+                variant={formik.errors.amount ? 'destructive' : 'primary'}
+                helperText={formik.errors.amount}
+            />
+
+            <Input
+                name='memo'
+                labelText={`${t('COMMON.MEMO')} (${t('COMMON.OPTIONAL')})`}
+                secondaryText={`${formik.values.memo !== undefined ? formik.values.memo.length : 0}/255`}
+                placeholder={t('COMMON.ADD_NOTE_TO_TRANSACTION')}
+                value={formik.values.memo}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                variant={formik.errors.memo ? 'destructive' : 'primary'}
+                helperText={formik.errors.memo}
+            />
+        </div>
+    );
+};
