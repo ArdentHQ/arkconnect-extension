d91dc5bd7090019d592cbd09520594e907b9fd6f
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-10 10:50:49 -0500

    fix: button variant

--- src/components/send/SendButton.tsx
+++ src/components/send/SendButton.tsx
@@ -10,7 +10,7 @@ export const SendButton = ({
 
     return (
         <div className='h-[84px] w-full bg-white p-4 shadow-button-container dark:bg-subtle-black dark:shadow-button-container-dark'>
-            <Button variant='secondary' disabled={disabled}>
+            <Button variant='primary' disabled={disabled}>
                 {t('COMMON.CONTINUE')}
             </Button>
         </div>
