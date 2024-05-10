23540490aed5b7c845943dd9f5111d16207d93c4
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-10 10:46:34 -0500

    feat: send form button

---
+++ src/components/send/SendButton.tsx
@@ -0,0 +1,18 @@
+import { useTranslation } from 'react-i18next';
+import { Button } from '@/shared/components';
+
+export const SendButton = ({
+    disabled
+}: {
+    disabled: boolean;
+}) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className='h-[84px] w-full bg-white p-4 shadow-button-container dark:bg-subtle-black dark:shadow-button-container-dark'>
+            <Button variant='secondary' disabled={disabled}>
+                {t('COMMON.CONTINUE')}
+            </Button>
+        </div>
+    );
+};
