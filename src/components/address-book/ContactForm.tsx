4464c895cdfe13ab00bb770f7d5132371e374dc0
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-13 08:28:17 -0500

    refactor: contact form component

---
+++ src/components/address-book/ContactForm.tsx
@@ -0,0 +1,46 @@
+import { useTranslation } from 'react-i18next';
+import { FormikProps } from 'formik';
+import { ContactFormik } from '@/components/address-book/types';
+import { Input, TextArea } from '@/shared/components';
+
+export const AddNewContactForm = ({
+    formik,
+    isLoading,
+}: {
+    formik: FormikProps<ContactFormik>;
+    isLoading: boolean;
+}) => {
+    const { t } = useTranslation();
+    const handleAddressChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        event.target.value = event.target.value.trim();
+        formik.handleChange(event);
+    };
+
+    return (
+        <div className='flex flex-col gap-4'>
+            <Input
+                type='text'
+                labelText={t('COMMON.NAME')}
+                placeholder={t('COMMON.ENTER_NAME')}
+                name='name'
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                variant={formik.errors.name ? 'destructive' : 'primary'}
+                helperText={formik.errors.name}
+            />
+            <TextArea
+                labelText={t('COMMON.ADDRESS')}
+                placeholder={t('COMMON.ENTER_ADDRESS')}
+                rows={2}
+                className='h-[72px]'
+                name='address'
+                value={formik.values.address}
+                onChange={handleAddressChange}
+                onBlur={formik.handleBlur}
+                variant={!isLoading && formik.errors.address ? 'destructive' : 'primary'}
+                helperText={isLoading ? undefined : formik.errors.address}
+            />
+        </div>
+    );
+};
