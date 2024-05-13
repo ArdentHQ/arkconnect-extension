afc385e6fe15baad4c9bd316cae42811a684f42d
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-13 08:29:05 -0500

    chore: update exports

--- src/components/address-book/index.ts
+++ src/components/address-book/index.ts
@@ -1,4 +1,6 @@
 export * from './AddContactButton';
 export * from './AddressBookList';
+export * from './ContactForm';
 export * from './NoContacts';
 export * from './RemoveAddress';
+export * from './SaveContactButton';
