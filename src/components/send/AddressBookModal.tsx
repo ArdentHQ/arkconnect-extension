4a26392045b1a7a59edebe9305788fb8a6403e4a
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-14 09:10:35 -0500

    feat: address book modal

--- src/components/send/AddressBookModal.tsx
+++ src/components/send/AddressBookModal.tsx
@@ -1,6 +1,7 @@
 import cn from 'classnames';
 import { Contact } from '@/lib/hooks/useAddressBook';
 import trimAddress from '@/lib/utils/trimAddress';
+import { Icon } from '@/shared/components';
 
 const ContactItem = ({ contact, onSelect, isSelected }: {
     contact: Contact;
@@ -9,19 +10,46 @@ const ContactItem = ({ contact, onSelect, isSelected }: {
 }) => {
     return (
         <button 
-            className={cn('w-[306px] h-[66px] py-3 px-4 border-b border-b-theme-secondary-200 hover:rounded-lg hover:bg-theme-secondary-50 hover:border-none flex flex-col gap-1 dark:bg-subtle-black dark:hover:bg-theme-secondary-700', {
-                '': isSelected,
+            className={cn('w-[306px] h-[66px] py-3 px-4 flex flex-row justify-between items-center transition-smoothEase', {
+                'rounded-lg bg-theme-primary-50 dark:bg-theme-primary-800/25': isSelected,
+                'border-b border-b-theme-secondary-200 hover:rounded-lg hover:bg-theme-secondary-50 dark:bg-light-black-black dark:hover:bg-theme-secondary-700 dark:border-b-theme-secondary-700 hover:border-transparent': !isSelected,
             })} 
             onClick={() => onSelect(contact.address)}
         >
-            <span className='text-light-black text-base font-medium leading-5 dark:text-white'>{contact.name}</span>
-            <span className='text-sm font-normal text-theme-secondary-500 dark:text-theme-secondary-300'>{trimAddress(contact.address, 10)}</span>
+            <span className='flex flex-col gap-1 items-start'>
+                <span className='text-light-black text-base font-medium leading-5 dark:text-white'>{contact.name}</span>
+                <span className='text-sm font-normal text-theme-secondary-500 dark:text-theme-secondary-300'>{trimAddress(contact.address, 10)}</span>
+            </span>
+            {
+                isSelected && <Icon icon='check' className='w-5 h-5 text-theme-primary-700 dark:text-theme-primary-600' />
+            }
         </button>
     );
 };
 
-export const AddressBookModal = () => {
-  return (
-    <div>AddressBookModal</div>
-  );
+export const AddressBookModal = ({
+    addressBook,
+    handleClick,
+    selectedAddress,
+}: {
+    addressBook: Contact[];
+    handleClick: (address: string) => void;
+    selectedAddress?: string;
+}) => {
+    return (
+        <div className='max-h-[330px] overflow-y-auto custom-scroll overflow-x-hidden'>
+            {
+                addressBook.map((contact: Contact, index) => {
+                    return (
+                        <ContactItem 
+                            key={index}
+                            contact={contact}
+                            onSelect={(address) => handleClick(address)}
+                            isSelected={selectedAddress === contact.address}
+                        />
+                    );
+                })
+            }
+        </div>
+    );
 };
