a7a309381d8019bb26ba70e06ac809bd0db9f236
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-19 16:09:02 -0500

    chore: update language files

--- src/i18n/ns/error.ts
+++ src/i18n/ns/error.ts
@@ -5,6 +5,7 @@ export default {
     IS_TOO_HIGH: '{{name}} is too high',
     IS_TOO_LONG: '{{name}} is too long',
     IS_REQUIRED: '{{name}} is required',
+    IS_EXCEEDING_BALANCE: 'The {{name}} is exceeding the balance',
     MAX_CHARACTERS: '{{count}} characters maximum',
     LEDGER_TRANSPORT_NOT_SUPPORTED: 'Ledger Transport is not supported!',
 };
