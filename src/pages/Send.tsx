c821f170eb543e62457203434a3a4c3e56d4fa0a
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-19 16:10:11 -0500

    refactor: add validation rules for fee

--- src/pages/Send.tsx
+++ src/pages/Send.tsx
@@ -8,6 +8,7 @@ import { usePrimaryWallet } from '@/lib/hooks/usePrimaryWallet';
 export type SendFormik = {
     amount?: string;
     memo?: string;
+    fee: string;
 };
 
 const Send = () => {
@@ -16,6 +17,7 @@ const Send = () => {
 
     const validationSchema = object().shape({
         amount: string()
+            .required(t('ERROR.IS_REQUIRED', { name: 'Amount' }))
             .matches(/^[0-9]+(\.[0-9]{1,8})?$/, {
                 message: t('ERROR.IS_INVALID', { name: 'Amount' }),
             })
@@ -23,23 +25,45 @@ const Send = () => {
                 if (!value) return true;
                 const userBalance = primaryWallet?.balance() || 0;
                 return Number(value) <= userBalance;
-            }),
+            })
+            .test('total-check', t('ERROR.IS_EXCEEDING_BALANCE', {name: 'fee + amount'}), (value) => {
+                if (!value || !formik.values.fee) return true;
+                const userBalance = primaryWallet?.balance() || 0;
+                const sum: number = Number(value) + Number(formik.values.fee);
+                return sum <= userBalance;
+            })
+            .trim(),
         memo: string().max(255, t('ERROR.IS_TOO_LONG', { name: 'Memo' })),
+        fee: string()
+            .required(t('ERROR.IS_REQUIRED', { name: 'Fee' }))
+            .matches(/^[0-9]+(\.[0-9]{1,8})?$/, {
+                message: t('ERROR.IS_INVALID', { name: 'Fee' }),
+            })
+            .test('min-value', t('ERROR.IS_REQUIRED', { name: 'Fee' }), (value) => {
+                return Number(value) > 0;
+            })
+            .test('max-value', t('ERROR.IS_TOO_HIGH', { name: 'Fee' }), (value) => {
+                return Number(value) < 1;
+            })
+            .trim(),
     });
 
     const formik = useFormik<SendFormik>({
         initialValues: {
             amount: '',
             memo: '',
+            fee: '',
         },
         validationSchema: validationSchema,
         onSubmit: () => {},
     });
 
     return (
-        <SubPageLayout title={t('COMMON.SEND')} className='relative'>
-            <SendForm formik={formik} />
-            <div className='absolute -bottom-4 left-0 w-full'>
+        <SubPageLayout title={t('COMMON.SEND')} className='relative p-0'>
+            <div className='custom-scroll h-[393px] w-full overflow-y-auto px-4'>
+                <SendForm formik={formik} />
+            </div>
+            <div className='w-full'>
                 <SendButton disabled={!(formik.isValid && formik.dirty)} />
             </div>
         </SubPageLayout>
