4fb7b7a32651f3597d41642e8e64414406f9e9c3
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-19 16:13:25 -0500

    feat: fee option list

---
+++ src/components/fees/FeeOptionsList.tsx
@@ -0,0 +1,39 @@
+import { useTranslation } from 'react-i18next';
+import { FeeOption, FeeOptionSkeleton } from './FeeOption';
+import { NetworkFee } from '@/lib/hooks/useNetworkFees';
+
+export const FeeOptionsList = ({
+    fee,
+    setFee,
+    fees,
+    isLoading = false,
+}: {
+    fee: string,
+    setFee: (fee: string) => void;
+    fees: NetworkFee['fees'];
+    isLoading?: boolean;
+}) => {
+    const { t } = useTranslation();
+
+    const handleClick = (fee: string) => {
+        setFee(fee);
+    };
+
+    if (!fees || isLoading) {
+        return (
+            <div className='w-full grid grid-cols-3 gap-1.5'>
+                <FeeOptionSkeleton />
+                <FeeOptionSkeleton />
+                <FeeOptionSkeleton />
+            </div>
+        );
+    }
+
+    return (
+        <div className='w-full grid grid-cols-3 gap-1.5'>
+            <FeeOption name={t('COMMON.SLOW')} value={fees.min.crypto} isSelected={fee == fees.min.crypto} onClick={handleClick} />
+            <FeeOption name={t('COMMON.AVERAGE')} value={fees.avg.crypto} isSelected={fee == fees.avg.crypto} onClick={handleClick} />
+            <FeeOption name={t('COMMON.FAST')} value={fees.max.crypto} isSelected={fee == fees.max.crypto} onClick={handleClick} />
+        </div>
+    );
+};
