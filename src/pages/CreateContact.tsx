5fcd52345a3adcb53fe3928baa1e660c53c3ba3f
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-05-27 15:06:44 +0000

    style: resolve style guide violations

--- src/pages/CreateContact.tsx
+++ src/pages/CreateContact.tsx
@@ -80,9 +80,13 @@ const CreateContact = () => {
             .test('valid-address', t('ERROR.IS_INVALID', { name: 'Address' }), () => {
                 return addressValidation.isValid;
             })
-            .test('unique-address', t('ERROR.IS_DUPLICATED', { name: 'contact address' }), (address) => {
-                return !addressBook?.find((contact) => contact.address === address);
-            }),
+            .test(
+                'unique-address',
+                t('ERROR.IS_DUPLICATED', { name: 'contact address' }),
+                (address) => {
+                    return !addressBook?.find((contact) => contact.address === address);
+                },
+            ),
     });
 
     const formik = useFormik<ContactFormik>({
--- src/pages/EditContact.tsx
+++ src/pages/EditContact.tsx
@@ -42,9 +42,13 @@ const EditContact = () => {
             .test('valid-address', t('ERROR.IS_INVALID', { name: 'Address' }), () => {
                 return addressValidation.isValid;
             })
-            .test('unique-address', t('ERROR.IS_DUPLICATED', { name: 'contact address' }), (address) => {
-                return !addressBook?.find((contact) => contact.address === address);
-            }),
+            .test(
+                'unique-address',
+                t('ERROR.IS_DUPLICATED', { name: 'contact address' }),
+                (address) => {
+                    return !addressBook?.find((contact) => contact.address === address);
+                },
+            ),
     });
 
     const formik = useFormik<ContactFormik>({
