3eb0342680d69aad80c2cbbf475d843a1489ef60
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-29 09:22:18 -0500

    feat: implement copy and download actions for qr code

--- src/components/receive/QRCodeContainer.tsx
+++ src/components/receive/QRCodeContainer.tsx
@@ -1,12 +1,17 @@
 import { useTranslation } from 'react-i18next';
 import QRCode from 'react-qr-code';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useFormik } from 'formik';
 import { object, string } from 'yup';
+import { toPng } from 'html-to-image';
+import { saveAs } from 'file-saver';
+import { QRActionButtons } from './QRActionButtons';
 import { usePrimaryWallet } from '@/lib/hooks/usePrimaryWallet';
 import { generateReceiveUrl } from '@/lib/utils/generateReceiveURL';
 import { Accordion, Input } from '@/shared/components';
 import constants from '@/constants';
+import useClipboard from '@/lib/hooks/useClipboard';
+import useToast from '@/lib/hooks/useToast';
 
 export type SendFormik = {
     amount?: string;
@@ -15,7 +20,10 @@ export type SendFormik = {
 
 export const QRCodeContainer = () => {
     const primaryWallet = usePrimaryWallet();
+    const toast = useToast();
+    const qrRef = useRef<HTMLDivElement | null>(null);
     const { t } = useTranslation();
+    const { copy } = useClipboard();
 
     const [isOpen, setIsOpen] = useState(false);
 
@@ -41,32 +49,56 @@ export const QRCodeContainer = () => {
         onSubmit: () => {},
     });
 
+    const generatedUrl = generateReceiveUrl({
+        coinName: primaryWallet?.network().coinName() ?? 'ARK',
+        netHash: primaryWallet?.network().meta().nethash,
+        address: primaryWallet?.address() ?? '',
+        amount:
+            !formik.errors.amount && formik.values.amount !== undefined
+                ? formik.values.amount
+                : undefined,
+        memo:
+            !formik.errors.memo && formik.values.memo !== undefined
+                ? formik.values.memo
+                : undefined,
+    });
+
+    const handleCopy = () => {
+        copy(generatedUrl, t('COMMON.QR_CODE'));
+    };
+
+    const handleDownload = () => {
+        if (qrRef.current) {
+            toPng(qrRef.current)
+              .then((dataUrl) => {
+                saveAs(dataUrl, 'qr-code.png');
+              });
+              toast('success', t('COMMON.HAS_BEEN_SAVED_SUCCESSFULLY', { name: 'QR Code' }));
+        }
+    };
+
     return (
         <div className='flex flex-col gap-4'>
             <div className='flex flex-col gap-1.5'>
                 <span className='text-sm font-medium text-theme-secondary-500 dark:text-theme-secondary-200'>
                     {t('COMMON.QR_CODE')}
                 </span>
-                <div className='flex w-full items-center justify-center rounded-lg border border-theme-secondary-200 bg-white px-3 py-4 dark:border-theme-secondary-600 dark:bg-theme-secondary-800 dark:text-theme-secondary-400 dark:shadow-secondary-dark'>
-                    <QRCode
-                        value={generateReceiveUrl({
-                            coinName: primaryWallet?.network().coinName() ?? 'ARK',
-                            netHash: primaryWallet?.network().meta().nethash,
-                            address: primaryWallet?.address() ?? '',
-                            amount:
-                                !formik.errors.amount && formik.values.amount !== undefined
-                                    ? formik.values.amount
-                                    : undefined,
-                            memo:
-                                !formik.errors.memo && formik.values.memo !== undefined
-                                    ? formik.values.memo
-                                    : undefined,
-                        })}
-                        size={200}
-                        bgColor={'#fff'}
-                        fgColor={'#000'}
-                        className='dark:rounded-md dark:bg-white dark:p-2'
-                    />
+                <div className='flex w-full items-center justify-center rounded-lg border border-theme-secondary-200 bg-white px-3 py-4 dark:border-theme-secondary-600 dark:bg-theme-secondary-800 dark:text-theme-secondary-400 dark:shadow-secondary-dark flex-col gap-4'>
+                    <div ref={qrRef}>
+                        <QRCode
+                            value={generatedUrl}
+                            size={200}
+                            bgColor={'#fff'}
+                            fgColor={'#000'}
+                            className='dark:rounded-md dark:bg-white dark:p-2'
+                        />
+                    </div>
+
+                    <div className='flex flex-row gap-2 items-center justify-center'>
+                        <QRActionButtons icon='download' text={t('COMMON.SAVE_with_name', { name: 'QR' })} onClick={handleDownload} />
+                        <hr className='h-5 w-px bg-theme-secondary-200 dark:bg-theme-secondary-600' />
+                        <QRActionButtons icon='copy' text={t('COMMON.COPY_with_name', { name: 'QR' })} onClick={handleCopy} />
+                    </div>
                 </div>
                 <span className='text-sm font-normal text-theme-secondary-500 dark:text-theme-secondary-300'>
                     {t('PAGES.RECEIVE.QR_CODE_WILL_BE_UPDATED_AUTOMATICALLY')}
