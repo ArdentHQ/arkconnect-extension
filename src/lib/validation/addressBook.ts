32f668418beea690b8f8f7ce69cd04177d8b0e60
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-05-30 07:24:54 -0500

    refactor: separate file to generate validation schema

---
+++ src/lib/validation/addressBook.ts
@@ -0,0 +1,45 @@
+import { TFunction } from 'i18next';
+import { object, string } from 'yup';
+import { ValidateAddressResponse } from '@/components/address-book/types';
+import { Contact } from '@/lib/hooks/useAddressBook';
+
+export const generateAddressBookValidationSchema = ({
+    isEdit = false, 
+    contact, 
+    addressBook = [], 
+    addressValidation,
+    t
+}: {
+    isEdit: boolean,
+    contact?: Contact,
+    addressBook: Contact[],
+    addressValidation: ValidateAddressResponse,
+    t: TFunction<'translation', undefined>
+}) => {
+    return object().shape({
+        name: string()
+            .required(t('ERROR.IS_REQUIRED', { name: 'Name' }))
+            .max(20, t('ERROR.MAX_CHARACTERS', { count: 20 }))
+            .test('unique-name', t('ERROR.IS_DUPLICATED', { name: 'contact name' }), (name) => {
+                if (isEdit && contact?.name === name) return true;
+                return !addressBook?.find((contact) => contact.name === name);
+            })
+            .matches(/^\S.*$/, t('ERROR.NO_LEADING_WHITESPACE', { name: 'Name' }))
+            .matches(/^.*\S$/, t('ERROR.NO_TRAILING_WHITESPACE', { name: 'Name' })),
+        address: string()
+            .required(t('ERROR.IS_REQUIRED', { name: 'Address' }))
+            .min(34, t('ERROR.IS_INVALID', { name: 'Address' }))
+            .max(34, t('ERROR.IS_INVALID', { name: 'Address' }))
+            .test('valid-address', t('ERROR.IS_INVALID', { name: 'Address' }), () => {
+                return addressValidation.isValid;
+            })
+            .test(
+                'unique-address',
+                t('ERROR.IS_DUPLICATED', { name: 'contact address' }),
+                (address) => {
+                    if (isEdit && contact?.address === address) return true;
+                    return !addressBook?.find((contact) => contact.address === address);
+                },
+            ),
+    });
+};
