e0130018a39db5544c7a28d43c2f64d4378fc218
ItsANameToo <ItsANameToo@users.noreply.github.com>
2024-05-30 10:05:24 +0000

    style: resolve style guide violations

--- src/pages/Send.tsx
+++ src/pages/Send.tsx
@@ -1,18 +1,18 @@
 import { object, string } from 'yup';
-import { SendButton, SendForm } from '@/components/send';
 import { useEffect, useState } from 'react';
 
 import { BigNumber } from '@ardenthq/sdk-helpers';
-import constants from '@/constants';
 import { runtime } from 'webextension-polyfill';
-import { ScreenName } from '@/lib/background/contracts';
-import SubPageLayout from '@/components/settings/SubPageLayout';
 import { useFormik } from 'formik';
 import { useNavigate } from 'react-router-dom';
-import { usePrimaryWallet } from '@/lib/hooks/usePrimaryWallet';
-import { useProfileContext } from '@/lib/context/Profile';
 import { useTranslation } from 'react-i18next';
 import { validateAddress } from './CreateContact';
+import constants from '@/constants';
+import { ScreenName } from '@/lib/background/contracts';
+import SubPageLayout from '@/components/settings/SubPageLayout';
+import { usePrimaryWallet } from '@/lib/hooks/usePrimaryWallet';
+import { useProfileContext } from '@/lib/context/Profile';
+import { SendButton, SendForm } from '@/components/send';
 import { ValidateAddressResponse } from '@/components/address-book/types';
 import { WalletNetwork } from '@/lib/store/wallet';
 
