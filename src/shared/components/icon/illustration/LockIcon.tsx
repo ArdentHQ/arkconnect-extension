840b2df926867011ebe9e4804874a8a31efed391
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-05-30 15:51:26 -0600

    one more

--- src/shared/components/icon/illustration/LockIcon.tsx
+++ src/shared/components/icon/illustration/LockIcon.tsx
@@ -2,6 +2,5 @@ import ThemedIcon from '@/shared/components/icon/ThemedIcon';
 
 export const LockIcon = () => {
     const icon = 'lock';
-
     return <ThemedIcon icon={icon} className='h-[190px] w-[190px]' />;
 };
