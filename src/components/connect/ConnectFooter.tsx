b5e954a21fbbebe6e258a1286ab17f1e83e2277f
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-06-02 16:48:19 -0500

    refactor: connection footer

--- src/components/connect/ConnectFooter.tsx
+++ src/components/connect/ConnectFooter.tsx
@@ -8,11 +8,11 @@ type ConnectFooterProps = {
 const ConnectFooter = ({ onSubmit, onCancel }: ConnectFooterProps) => {
     const { t } = useTranslation();
     return (
-        <div className='px-4'>
-            <HeadingDescription className='mb-8'>
+        <div>
+            <HeadingDescription className='mb-6 px-4'>
                 {t('PAGES.CONNECT.CAN_SEE_YOUR_ADDRESS_DISCLAIMER')}
             </HeadingDescription>
-            <div className='grid grid-cols-2 gap-2'>
+            <div className='grid grid-cols-2 gap-2 px-4 w-full h-full items-center dark:bg-subtle-black shadow-button-container dark:shadow-button-container-dark bg-white py-4'>
                 <Button variant='secondaryBlack' onClick={onCancel}>
                     {t('ACTION.REFUSE')}
                 </Button>
