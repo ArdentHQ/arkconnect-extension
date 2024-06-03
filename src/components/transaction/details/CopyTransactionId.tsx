79d73215b1eafb2358066ba5b25a0b6138bb395b
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-06-03 15:06:10 +0000

    style: resolve style guide violations

--- src/components/transaction/details/CopyTransactionId.tsx
+++ src/components/transaction/details/CopyTransactionId.tsx
@@ -2,26 +2,18 @@ import { useTranslation } from 'react-i18next';
 import useClipboard from '@/lib/hooks/useClipboard';
 import { Icon, Tooltip } from '@/shared/components';
 
-
-export const CopyTransactionId = ({
-    transactionId
-}: {
-    transactionId: string;
-}) => {
+export const CopyTransactionId = ({ transactionId }: { transactionId: string }) => {
     const { t } = useTranslation();
     const { copy } = useClipboard();
 
     return (
-        <Tooltip content={t('COMMON.COPY_with_name', {name: 'TxID'})}>
+        <Tooltip content={t('COMMON.COPY_with_name', { name: 'TxID' })}>
             <button
                 type='button'
-                className='block p-2 rounded-full dark:hover:bg-theme-secondary-700 bg-transparent transition-smoothEase dark:text-white text-light-black hover:bg-theme-secondary-100'
+                className='transition-smoothEase block rounded-full bg-transparent p-2 text-light-black hover:bg-theme-secondary-100 dark:text-white dark:hover:bg-theme-secondary-700'
                 onClick={() => copy(transactionId, t('COMMON.TRANSACTION_ID'))}
             >
-                <Icon
-                    icon='copy'
-                    className='h-4 w-4'
-                />
+                <Icon icon='copy' className='h-4 w-4' />
             </button>
         </Tooltip>
     );
