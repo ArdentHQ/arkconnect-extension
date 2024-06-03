e21226a908af8e5da378249dcb7971ddc8923460
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-06-02 20:42:51 -0500

    refactor: vote approved page

--- src/pages/VoteApproved.tsx
+++ src/pages/VoteApproved.tsx
@@ -43,53 +43,59 @@ const VoteApproved = () => {
     };
 
     return (
-        <div className='left-0 top-0 z-10 flex w-full flex-col items-center justify-center bg-subtle-white dark:bg-light-black'>
-            <RequestedBy appDomain={formatDomain(session.domain) || ''} appLogo={session.logo} />
-            <div className='flex w-full flex-col items-center justify-between gap-[37px] px-4 py-6'>
-                <div className='flex w-full flex-col items-center gap-6'>
-                    <div className='flex flex-row items-center justify-center gap-3'>
-                        {isTransactionConfirmed ? (
-                            <Icon
-                                icon='completed'
-                                className='h-6 w-6 text-theme-primary-700 dark:text-theme-primary-650'
-                            />
-                        ) : (
-                            <div className='flex h-6 w-6 items-center justify-center rounded-full bg-theme-primary-700 dark:bg-theme-primary-650'>
-                                <Icon
-                                    icon='pending'
-                                    className='h-4 w-4 text-theme-primary-700 dark:text-theme-primary-650'
-                                />
-                            </div>
-                        )}
-                        <Heading level={3}>
-                            {isTransactionConfirmed ? getTitle() : t('PAGES.PENDING_CONFIRMATION')}
-                        </Heading>
-                    </div>
-                    <ActionBody
-                        isApproved
-                        wallet={wallet}
-                        sender={trimAddress(state?.vote.sender ?? '', 10)}
-                        showFiat={showFiat}
-                        fee={state?.vote.fee}
-                        convertedFee={state?.vote.convertedFee as number}
-                        exchangeCurrency={state?.vote.exchangeCurrency as string}
-                        network={getActiveCoin(state?.walletNetwork)}
-                        unvote={{
-                            name: state?.vote.unvoteName,
-                            publicKey: state?.vote.unvotePublicKey,
-                            address: state?.vote.unvoteAddress,
-                        }}
-                        vote={{
-                            name: state?.vote.voteName,
-                            publicKey: state?.vote.votePublicKey,
-                            address: state?.vote.voteAddress,
-                        }}
-                        transactionId={isTransactionConfirmed ? vote.id : undefined}
-                        actionDetailsClassName='max-h-81.5'
-                    />
-                </div>
+        <div className='left-0 top-0 z-10 flex w-full flex-col items-center justify-center bg-subtle-white dark:bg-light-black h-screen'>
+            <div className='flex-none w-full'>
+                <RequestedBy appDomain={formatDomain(session.domain) || ''} appLogo={session.logo} />
+            </div>
 
-                <div className='flex w-full flex-col gap-5'>
+            <div className='flex-1 overflow-y-auto custom-scroll w-full'>
+                <div className='flex w-full flex-col items-center justify-between gap-[37px] px-4 py-6'>
+                    <div className='flex w-full flex-col items-center gap-4'>
+                        <div className='flex flex-row items-center justify-center gap-3'>
+                            {isTransactionConfirmed ? (
+                                <Icon
+                                    icon='completed'
+                                    className='h-6 w-6 text-theme-primary-700 dark:text-theme-primary-650'
+                                />
+                            ) : (
+                                <div className='flex h-6 w-6 items-center justify-center rounded-full bg-theme-primary-700 dark:bg-theme-primary-650'>
+                                    <Icon
+                                        icon='pending'
+                                        className='h-4 w-4 text-theme-primary-700 dark:text-theme-primary-650'
+                                    />
+                                </div>
+                            )}
+                            <Heading level={3}>
+                                {isTransactionConfirmed ? getTitle() : t('PAGES.PENDING_CONFIRMATION')}
+                            </Heading>
+                        </div>
+                        <ActionBody
+                            isApproved
+                            wallet={wallet}
+                            sender={trimAddress(state?.vote.sender ?? '', 10)}
+                            showFiat={showFiat}
+                            fee={state?.vote.fee}
+                            convertedFee={state?.vote.convertedFee as number}
+                            exchangeCurrency={state?.vote.exchangeCurrency as string}
+                            network={getActiveCoin(state?.walletNetwork)}
+                            unvote={{
+                                name: state?.vote.unvoteName,
+                                publicKey: state?.vote.unvotePublicKey,
+                                address: state?.vote.unvoteAddress,
+                            }}
+                            vote={{
+                                name: state?.vote.voteName,
+                                publicKey: state?.vote.votePublicKey,
+                                address: state?.vote.voteAddress,
+                            }}
+                            transactionId={isTransactionConfirmed ? vote.id : undefined}
+                        />
+                    </div>
+                </div>
+            </div>
+            
+            <div className='flex-none w-full'>
+                <div className='flex w-full flex-col gap-5 p-4 dark:bg-subtle-black shadow-button-container dark:shadow-button-container-dark bg-white'>
                     <Button variant='primary' onClick={onClose}>
                         {t('ACTION.CLOSE')}
                     </Button>
