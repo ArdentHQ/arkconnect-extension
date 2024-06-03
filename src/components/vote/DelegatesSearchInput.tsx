01cc782ab36082936aad42be1354078431a412dc
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-06-03 16:36:21 -0600

    add clear button

--- src/components/vote/DelegatesSearchInput.tsx
+++ src/components/vote/DelegatesSearchInput.tsx
@@ -9,14 +9,23 @@ export const DelegatesSearchInput = ({
     setSearchQuery: Dispatch<SetStateAction<string>>;
 }) => {
     return (
-        <div className='relative'>
+        <div className='-mt-1.5 mb-2'>
             <Input
-                className='border-theme-secondary-400 pl-10 placeholder:text-theme-secondary-500 hover:bg-theme-secondary-50 dark:border-theme-secondary-300 dark:bg-subtle-black dark:placeholder:text-theme-secondary-300 dark:hover:bg-theme-secondary-700'
+                value={searchQuery}
+                onChange={(event) => setSearchQuery(event.target.value)}
+                className='border-theme-secondary-400 pl-10 placeholder:text-theme-secondary-500 hover:bg-theme-secondary-50 focus:border-black dark:border-theme-secondary-300 dark:bg-subtle-black dark:placeholder:text-theme-secondary-300 dark:hover:bg-theme-secondary-700 dark:focus:border-theme-secondary-300'
                 leading={
                     <Icon
                         icon='search'
                         className='h-6 w-6 text-theme-secondary-500 dark:text-theme-secondary-300'
                     />
+                }
+                trailing={
+                    searchQuery !== '' && (
+                        <button className='mt-1.5' type='button' onClick={() => setSearchQuery('')}>
+                            <Icon icon='clear' className='h-5 w-5 text-black dark:text-white' />
+                        </button>
+                    )
                 }
                 placeholder='Find a delegate (add i18n)'
             />
