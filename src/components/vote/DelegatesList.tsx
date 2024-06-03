c7205a4515c222a700617a48071f9bca49cb6953
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-06-03 13:11:54 -0600

    add loading skeleton

--- src/components/vote/DelegatesList.tsx
+++ src/components/vote/DelegatesList.tsx
@@ -1,5 +1,6 @@
 import { Contracts } from '@ardenthq/sdk-profiles';
 import { DelegatesListItem } from './DelegatesListItem';
+import { DelegatesListItemSkeleton } from './DelegatesListItemSkeleton';
 
 export const DelegatesList = ({
     delegates,
@@ -8,9 +9,23 @@ export const DelegatesList = ({
     delegates: Contracts.IReadOnlyWallet[];
     isLoading: boolean;
 }) => {
+    if (isLoading) {
+        return (
+            <div className='w-full overflow-hidden rounded-xl bg-white py-2 dark:bg-subtle-black'>
+                <table className='w-full'>
+                    <tbody>
+                        {Array.from({ length: 10 }).map((_, index) => {
+                            return <DelegatesListItemSkeleton key={index} />;
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
     return (
         <div className='w-full overflow-hidden rounded-xl bg-white py-2 dark:bg-subtle-black'>
-            <table>
+            <table className='w-full'>
                 <tbody>
                     {delegates.map((delegate) => {
                         return <DelegatesListItem key={delegate.address()} delegate={delegate} />;
