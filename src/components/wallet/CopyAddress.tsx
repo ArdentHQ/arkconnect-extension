c15fde99787487525bf4b7bcc3e53d291bb244cd
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-06-04 07:25:52 -0500

    refactor: copy address button

--- src/components/wallet/CopyAddress.tsx
+++ src/components/wallet/CopyAddress.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import { Icon, Tooltip } from '@/shared/components';
 import { usePrimaryWallet } from '@/lib/hooks/usePrimaryWallet';
 import useClipboard from '@/lib/hooks/useClipboard';
@@ -5,8 +6,13 @@ import trimAddress from '@/lib/utils/trimAddress';
 import { ToastPosition } from '@/components/toast/ToastContainer';
 import { HeaderButton } from '@/shared/components/header/HeaderButton';
 
-export const CopyAddress = () => {
+export const CopyAddress = ({
+    disableTooltip = false,
+}: {
+    disableTooltip?: boolean;
+}) => {
     const primaryWallet = usePrimaryWallet();
+    const { t } = useTranslation();
 
     const { copy } = useClipboard();
 
@@ -17,7 +23,7 @@ export const CopyAddress = () => {
     const trimmedAddress = trimAddress(primaryWallet.address(), 10);
 
     return (
-        <Tooltip content='Copy address' placement='bottom-end'>
+        <Tooltip content={t('COMMON.COPY_with_name', { name: 'Address'})} placement='bottom-end' disabled={disableTooltip}>
             <HeaderButton
                 className='rounded-full'
                 onClick={() => {
