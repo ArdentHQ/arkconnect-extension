532994b4350e5ce76e6123a0554aece9ae7ef15f
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-06-04 07:26:18 -0500

    fix: adjust hover effect for copy address button

--- src/components/receive/Recipient.tsx
+++ src/components/receive/Recipient.tsx
@@ -1,15 +1,12 @@
 import { useTranslation } from 'react-i18next';
 import { TransactionAddress } from '../transaction/Transaction.blocks';
+import { CopyAddress } from '../wallet/CopyAddress';
 import { usePrimaryWallet } from '@/lib/hooks/usePrimaryWallet';
-import useClipboard from '@/lib/hooks/useClipboard';
-import trimAddress from '@/lib/utils/trimAddress';
-import { Icon } from '@/shared/components';
 
 export const Recipient = () => {
     const primaryWallet = usePrimaryWallet();
     const address = primaryWallet?.address() ?? '';
     const { t } = useTranslation();
-    const { copy } = useClipboard();
 
     return (
         <div className='flex flex-col gap-1.5'>
@@ -18,16 +15,7 @@ export const Recipient = () => {
             </span>
             <div className='flex w-full items-center justify-between rounded-lg border border-theme-secondary-200 bg-white px-3 py-4 dark:border-theme-secondary-600 dark:bg-theme-secondary-800 dark:text-theme-secondary-400 dark:shadow-secondary-dark'>
                 <TransactionAddress address={address} displayParenthesis />
-                <button
-                    type='button'
-                    className='block'
-                    onClick={() => copy(address, trimAddress(address, 'short'))}
-                >
-                    <Icon
-                        icon='copy'
-                        className='h-5 w-5 text-theme-primary-700 dark:text-theme-primary-600'
-                    />
-                </button>
+                <CopyAddress disableTooltip/>
             </div>
         </div>
     );
