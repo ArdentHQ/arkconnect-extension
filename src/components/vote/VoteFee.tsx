7ca8e33d39d022923a555a8089c03a799727665e
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-06-05 14:22:06 -0600

    add edit fee button

--- src/components/vote/VoteFee.tsx
+++ src/components/vote/VoteFee.tsx
@@ -1,23 +1,32 @@
-import { Input } from '@/shared/components';
-
 export const VoteFee = ({ delegateAddress }: { delegateAddress: string }) => {
-    if (delegateAddress === '') {
-        return (
-            <div className='flex h-[42px] items-center justify-between rounded-lg border border-theme-secondary-400 p-3 text-sm  text-theme-secondary-500 dark:border-theme-secondary-500'>
-                <span className='text-sm font-medium dark:text-theme-secondary-200'>
-                    Transaction Fee
-                </span>
+    const disabled = delegateAddress === '';
+    return (
+        <div className='flex h-[42px] items-center justify-between rounded-lg border border-theme-secondary-400 p-3 text-sm text-theme-secondary-500 dark:border-theme-secondary-500'>
+            <span className='text-sm font-medium dark:text-theme-secondary-200'>
+                Transaction Fee
+            </span>
 
-                <div className='flex items-center space-x-1.5 dark:text-theme-secondary-500'>
+            <div className='flex items-center space-x-1.5 dark:text-theme-secondary-500'>
+                {disabled ? (
                     <span>- ARK</span>
+                ) : (
+                    // @TODO: remove hardcoded value
+                    <span className='font-medium text-black'>0.60 ARK</span>
+                )}
 
-                    <span className='h-1 w-1 rounded-full bg-theme-secondary-400'></span>
+                <span className='h-1 w-1 rounded-full bg-theme-secondary-400'></span>
 
+                {disabled ? (
                     <span className='font-medium'>Edit Fee</span>
-                </div>
+                ) : (
+                    <button
+                        type='button'
+                        className='transition-smoothEase font-medium text-theme-primary-700 hover:text-theme-primary-600 dark:text-theme-primary-600 dark:hover:text-theme-primary-650'
+                    >
+                        Edit Fee
+                    </button>
+                )}
             </div>
-        );
-    }
-
-    return <Input />;
+        </div>
+    );
 };
