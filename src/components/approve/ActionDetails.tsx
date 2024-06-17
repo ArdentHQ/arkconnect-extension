39d693797fdd148276555f1e4cab7e7e3820bb4e
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-06-17 09:41:03 -0500

    refactor: allow custom class names in action transaction id row

--- src/components/approve/ActionDetails.tsx
+++ src/components/approve/ActionDetails.tsx
@@ -5,14 +5,16 @@ export const ActionDetailsRow = ({
     label,
     children,
     below,
+    className,
 }: {
     label: string | React.ReactNode;
     children: string | React.ReactNode;
     below?: React.ReactNode;
+    className?: string;
 }) => {
     return (
         <div className='flex flex-col space-y-1 border-b border-solid border-b-theme-secondary-100 p-3 last:border-b-0 dark:border-b-theme-secondary-700'>
-            <div className='flex justify-between'>
+            <div className={cn('flex justify-between', className)}>
                 <div className='text-sm text-theme-secondary-500 dark:text-theme-secondary-300'>
                     {label}
                 </div>
