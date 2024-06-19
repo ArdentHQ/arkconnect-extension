ccdeb8fb2bd682888d10647fa0eaba5a5bbbe7f0
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-06-19 14:05:55 -0500

    refactor: requested signature message

--- src/components/approve/RequestedSignatureMessage.tsx
+++ src/components/approve/RequestedSignatureMessage.tsx
@@ -1,10 +1,12 @@
 import { useTranslation } from 'react-i18next';
+import { twMerge } from 'tailwind-merge';
 
 type Props = {
     data: { [key: string]: number | string };
+    className?: string;
 };
 
-const RequestedSignatureMessage = ({ data }: Props) => {
+const RequestedSignatureMessage = ({ data, className }: Props) => {
     const { t } = useTranslation();
 
     return (
@@ -13,7 +15,7 @@ const RequestedSignatureMessage = ({ data }: Props) => {
                 {t('COMMON.MESSAGE')}
             </div>
 
-            <div className='custom-scroll flex min-h-60 w-full flex-1 overflow-auto rounded-lg border border-solid border-theme-secondary-200 bg-white p-3 text-light-black dark:border-theme-secondary-700 dark:bg-subtle-black dark:text-white'>
+            <div className={twMerge('custom-scroll flex min-h-60 w-full flex-1 overflow-auto rounded-lg border border-solid border-theme-secondary-200 bg-white p-3 text-light-black dark:border-theme-secondary-700 dark:bg-subtle-black dark:text-white', className)}>
                 {data.message}
             </div>
         </div>
