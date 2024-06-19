399d54b33c1501415d677c3ac9ac879d6eb5a871
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-06-19 14:06:11 -0500

    fix: remove fixed height for signature message

--- src/components/ledger/ApproveWithLedger.blocks.tsx
+++ src/components/ledger/ApproveWithLedger.blocks.tsx
@@ -67,7 +67,6 @@ export const VoteLedgerApprovalBody = ({ wallet, state }: Props) => {
                 publicKey: vote?.wallet?.publicKey(),
                 address: vote?.wallet?.address(),
             }}
-            actionDetailsClassName='max-h-81.5'
             hasHigherCustomFee={hasHigherCustomFee}
             amountTicker={wallet.currency()}
         />
@@ -118,8 +117,6 @@ export const SignatureLedgerApprovalBody = () => {
     const { state } = location;
 
     return (
-        <div className='h-[191px]'>
-            <RequestedSignatureMessage data={state} />
-        </div>
+        <RequestedSignatureMessage data={state} className='min-h-[194px]' />
     );
 };
