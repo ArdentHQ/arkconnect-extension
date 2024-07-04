2eb0deeebaa8f2629deb2533d4ff2d927ff9c444
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-07-04 13:24:47 +0000

    style: resolve style guide violations

--- src/lib/hooks/useOs.ts
+++ src/lib/hooks/useOs.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { runtime } from 'webextension-polyfill';
 import constants from '@/constants';
 
-export const useOs = (): { os: string }  => {
+export const useOs = (): { os: string } => {
     const [os, setOs] = useState<string>(constants.DEFAULT_OS);
 
     useEffect(() => {
