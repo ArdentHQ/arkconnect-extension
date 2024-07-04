ed9a456e6776f297c701fe35b4ac2dc1a8da9b3e
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-07-04 07:03:37 -0500

    fix: maintain color of infobanner when changing theme

--- src/shared/components/utils/InfoBanner.tsx
+++ src/shared/components/utils/InfoBanner.tsx
@@ -2,11 +2,11 @@ import { ReactNode } from 'react';
 
 const InfoBanner = ({ title, children }: { title: string; children: ReactNode }) => {
     return (
-        <div className='flex flex-col overflow-hidden rounded-2.5xl border border-theme-primary-200 dark:border-theme-primary-700'>
-            <div className='bg-theme-primary-100 px-4 pb-3 pt-4 text-sm font-medium text-subtle-black dark:bg-theme-primary-700 dark:text-white'>
+        <div className='flex flex-col overflow-hidden rounded-2.5xl border border-theme-navy-200 dark:border-theme-navy-700'>
+            <div className='bg-theme-navy-100 px-4 pb-3 pt-4 text-sm font-medium text-subtle-black dark:bg-theme-navy-700 dark:text-white'>
                 {title}
             </div>
-            <div className='bg-theme-primary-50 px-4 pb-4 pt-3 text-xs font-medium leading-4 text-theme-primary-800 dark:bg-theme-primary-900 dark:text-subtle-white'>
+            <div className='bg-theme-navy-50 px-4 pb-4 pt-3 text-xs font-medium leading-4 text-theme-navy-800 dark:bg-theme-navy-900 dark:text-subtle-white'>
                 {children}
             </div>
         </div>
