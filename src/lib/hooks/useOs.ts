a7a270ee59a71281c47228aa4f95792fe64fb9e9
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-07-04 08:20:24 -0500

    feat: use os hook

---
+++ src/lib/hooks/useOs.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+import { runtime } from 'webextension-polyfill';
+import constants from '@/constants';
+
+export const useOs = (): { os: string }  => {
+    const [os, setOs] = useState<string>(constants.DEFAULT_OS);
+
+    useEffect(() => {
+        const fetchPlatformInfo = async () => {
+            const platformInfo = await runtime.getPlatformInfo();
+            setOs(platformInfo.os);
+        };
+
+        fetchPlatformInfo();
+    }, []);
+
+    return { os };
+};
