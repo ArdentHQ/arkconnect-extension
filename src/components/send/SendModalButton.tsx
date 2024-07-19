40f36bc54734788f31227f096f5a69a7ab36985d
Patricio Marroquin <55117912+patricio0312rev@users.noreply.github.com>
2024-07-19 10:18:56 -0500

    refactor: disable qr upload button on firefox (#445)

--- src/components/send/SendModalButton.tsx
+++ src/components/send/SendModalButton.tsx
@@ -1,20 +1,30 @@
 import { MouseEventHandler } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button } from '@/shared/components';
+import { Button, Tooltip } from '@/shared/components';
+import { isFirefox } from '@/lib/utils/isFirefox';
 
 const SendModalButton = ({ onClick }: { onClick: MouseEventHandler<HTMLButtonElement> }) => {
     const { t } = useTranslation();
 
     return (
-        <Button
-            variant='secondaryBlack'
-            className='!h-fit !w-fit !px-3 !py-1.5 !text-sm !font-medium'
-            iconTrailing='qr-code'
-            iconClass='h-4 w-4'
-            onClick={onClick}
+        <Tooltip
+            content={t('PAGES.SEND.QR_MODAL.FIREFOX_NOT_SUPPORTED')}
+            disabled={!isFirefox}
+            className='w-60'
         >
-            {t('PAGES.SEND.QR_MODAL.UPLOAD_QR')}
-        </Button>
+            <div>
+                <Button
+                    variant='secondaryBlack'
+                    className='!h-fit !w-fit !px-3 !py-1.5 !text-sm !font-medium'
+                    iconTrailing='qr-code'
+                    iconClass='h-4 w-4'
+                    onClick={onClick}
+                    disabled={isFirefox}
+                >
+                    {t('PAGES.SEND.QR_MODAL.UPLOAD_QR')}
+                </Button>
+            </div>
+        </Tooltip>
     );
 };
 
