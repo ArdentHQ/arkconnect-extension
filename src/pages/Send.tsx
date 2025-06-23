e1fde4a61685bdee4ef6bf0f57a4700ee629fac5
shahin-hq <shahin-hq@users.noreply.github.com>
2025-06-23 15:42:44 +0000

    style: resolve style guide violations

--- src/pages/Send.tsx
+++ src/pages/Send.tsx
@@ -87,9 +87,7 @@ const Send = () => {
                     const userBalance = BigNumber.make(primaryWallet?.balance() || 0);
                     const fee = calculateGasFee(formik.values.gasPrice, formik.values.gasLimit);
 
-                    const sum: BigNumber = BigNumber.make(value).plus(
-                        BigNumber.make(fee),
-                    );
+                    const sum: BigNumber = BigNumber.make(value).plus(BigNumber.make(fee));
                     return sum.isLessThanOrEqualTo(userBalance);
                 },
             )
@@ -179,7 +177,6 @@ const Send = () => {
             });
         },
     });
-
 
     const { receiverAddress, gasLimit, gasPrice, amount } = formik.values;
 
