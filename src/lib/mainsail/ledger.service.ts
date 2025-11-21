91aad311018373fa60929bede7508c9b793c6b45
Shahin Safaraliyev <shahin@ardenthq.com>
2025-11-21 16:49:10 +0400

    wip

--- src/lib/mainsail/ledger.service.ts
+++ src/lib/mainsail/ledger.service.ts
@@ -1,6 +1,6 @@
 import { Contracts, Services } from "@/app/lib/mainsail";
 import { BIP44, HDKey } from "@ardenthq/arkvault-crypto";
-import { connectedTransport as ledgerTransportFactory } from "@/app/contexts/Ledger/transport";
+import { connectedTransport as ledgerTransportFactory } from "@/lib/utils/transport";
 
 import { createRange } from "./ledger.service.helpers.js";
 import { LedgerSignature } from "./ledger.service.types.js";
