1dda54b30e95eb56f667bc4e2960fb8a1f71d001
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-05-01 01:08:56 +0100

    feat: tokens tabs

--- src/components/home/LatestTransactions.blocks.tsx
+++ src/components/home/LatestTransactions.blocks.tsx
@@ -2,13 +2,6 @@ import cn from 'classnames';
 import dayjs from 'dayjs';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
-import { TransactionAmount } from '../transaction/Transaction.blocks';
-import {
-    getTransactionIcon,
-    getType,
-    getUniqueRecipients,
-    TransactionType,
-} from './LatestTransactions.utils';
 import { Button, EmptyConnectionsIcon, ExternalLink, Icon, Tooltip } from '@/shared/components';
 
 import { getExplorerDomain } from '@/lib/utils/networkUtils';
@@ -20,6 +13,13 @@ import { usePrimaryWallet } from '@/lib/hooks/usePrimaryWallet';
 import { isFirefox } from '@/lib/utils/isFirefox';
 import { ExtendedConfirmedTransactionData } from '@/lib/profiles/transaction.dto';
 import { IReadWriteWallet } from '@/lib/profiles/wallet.contract';
+import { TransactionAmount } from '../transaction/Transaction.blocks';
+import {
+    getTransactionIcon,
+    getType,
+    getUniqueRecipients,
+    TransactionType,
+} from './LatestTransactions.utils';
 
 export const TransactionTitle = ({
     type,
--- src/components/home/LatestTransactions.tsx
+++ src/components/home/LatestTransactions.tsx
@@ -1,11 +1,13 @@
 import { useTranslation } from 'react-i18next';
-import { useEffect } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useQuery } from 'react-query';
-import { NoTransactions, TransactionsList } from './LatestTransactions.blocks';
 import { usePrimaryWallet } from '@/lib/hooks/usePrimaryWallet';
 import { Loader } from '@/shared/components';
 import { ExtendedConfirmedTransactionData } from '@/lib/profiles/transaction.dto';
 import { IReadWriteWallet } from '@/lib/profiles/wallet.contract';
+import classNames from 'classnames';
+import { NoTransactions, TransactionsList } from './LatestTransactions.blocks';
+import { TransactionsTabs, TransactionTab } from './TransactionsTabs';
 
 type TransactionResponse = {
     transactions: ExtendedConfirmedTransactionData[];
@@ -27,9 +29,18 @@ const fetchTransactions = async (
     }
 };
 
+// TODO: Implement fetching tokens when token index is implemented
+const fetchTokens = async (): Promise<TransactionResponse> => {
+    return {
+        transactions: [],
+        hasMorePages: false,
+    };
+};
+
 export const LatestTransactions = () => {
     const { t } = useTranslation();
     const primaryWallet = usePrimaryWallet();
+    const [activeTab, setActiveTab] = useState<string>('TOKENS');
 
     const { data, refetch, isLoading } = useQuery<TransactionResponse>(
         ['transactions', primaryWallet?.address()],
@@ -41,37 +52,76 @@ export const LatestTransactions = () => {
         },
     );
 
+    const {
+        data: tokenData,
+        refetch: refetchTokens,
+        isLoading: isLoadingTokens,
+    } = useQuery<TransactionResponse>(['tokens', primaryWallet?.address()], () => fetchTokens(), {
+        enabled: !!primaryWallet,
+        staleTime: 0,
+        refetchInterval: 60000,
+    });
+
+    const tabs = useMemo(() => {
+        if (tokenData && tokenData.transactions.length > 0) {
+            return ['TOKENS', 'TRANSACTIONS'];
+        }
+
+        return ['TRANSACTIONS'];
+    }, [tokenData]);
+
     useEffect(() => {
         if (primaryWallet) {
             refetch();
+            refetchTokens();
         }
-    }, [primaryWallet, refetch]);
+    }, [primaryWallet, refetch, refetchTokens]);
+
+    const showTabs = !isLoadingTokens && tabs.length > 1;
 
     return (
-        <div className='mt-4 h-full w-full rounded-t-2xl bg-white dark:bg-subtle-black'>
-            <div className='border-b border-b-theme-secondary-200 p-4 text-lg font-medium leading-tight text-light-black dark:border-b-theme-secondary-600 dark:text-white'>
-                {t('PAGES.HOME.LATEST_TRANSACTIONS')}
-            </div>
-
-            {!isLoading && data ? (
-                <div className='h-auto w-full'>
-                    {data.transactions.length > 0 ? (
-                        <TransactionsList
-                            transactions={data.transactions}
-                            displayButton={data.hasMorePages}
-                        />
-                    ) : (
-                        <NoTransactions />
-                    )}
-                </div>
-            ) : (
-                <div className='flex h-[270px] w-full items-center justify-center'>
-                    <Loader
-                        variant='big'
-                        className='dark:border-theme-secondary-700 dark:border-t-theme-primary-650'
-                    />
-                </div>
+        <div className={classNames(['h-full w-full'], { 'mt-4': !showTabs })}>
+            {showTabs && (
+                <TransactionsTabs>
+                    {tabs.map((tab) => (
+                        <TransactionTab
+                            key={tab}
+                            active={activeTab === tab}
+                            onClick={() => setActiveTab(tab)}
+                        >
+                            {t(`PAGES.HOME.TABS.${tab}`)}
+                        </TransactionTab>
+                    ))}
+                </TransactionsTabs>
             )}
+
+            <div className='h-full w-full rounded-t-xl bg-white dark:bg-subtle-black'>
+                {!showTabs && (
+                    <div className='border-b border-b-theme-secondary-200 p-4 text-lg font-medium leading-tight text-light-black dark:border-b-theme-secondary-600 dark:text-white'>
+                        {t('PAGES.HOME.LATEST_TRANSACTIONS')}
+                    </div>
+                )}
+
+                {!isLoading && data ? (
+                    <div className='h-auto w-full'>
+                        {data.transactions.length > 0 ? (
+                            <TransactionsList
+                                transactions={data.transactions}
+                                displayButton={data.hasMorePages}
+                            />
+                        ) : (
+                            <NoTransactions />
+                        )}
+                    </div>
+                ) : (
+                    <div className='flex h-[270px] w-full items-center justify-center'>
+                        <Loader
+                            variant='big'
+                            className='dark:border-theme-secondary-700 dark:border-t-theme-primary-650'
+                        />
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
