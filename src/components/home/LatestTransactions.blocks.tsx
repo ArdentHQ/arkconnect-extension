be895b1845c6daa76604951ef09cfd7cbd0e5de9
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-05-20 10:39:01 -0500

    fix: double scrollbar on home

--- src/components/home/LatestTransactions.blocks.tsx
+++ src/components/home/LatestTransactions.blocks.tsx
@@ -244,7 +244,7 @@ const TransactionListItem = ({
 export const TransactionsList = ({
     transactions,
     displayButton,
-    maxHeight = 'max-h-[237px]',
+    maxHeight = 'max-h-[235px]',
 }: {
     transactions: ExtendedConfirmedTransactionData[];
     displayButton: boolean;
@@ -345,7 +345,7 @@ export const TokensList = ({ tokens }: { tokens: WalletToken[] }) => {
 
     return (
         <div className='flex flex-col'>
-            <div className='custom-scroll max-h-[237px] overflow-auto'>
+            <div className='custom-scroll max-h-[235px] overflow-auto'>
                 {tokens.map((token) => (
                     <TokenListItem key={token.token().address()} token={token} />
                 ))}
