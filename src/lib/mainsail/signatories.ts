4e9a8c7f212e1a448b6a9d8864907c0458226f73
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-05-26 03:31:01 -0600

    refactor: collapse signatory classes into one (#516)

--- src/lib/mainsail/signatories.ts
+++ src/lib/mainsail/signatories.ts
@@ -1,7 +1,2 @@
-export { ConfirmationMnemonicSignatory } from "./confirmation-mnemonic.signatory";
-export { ConfirmationSecretSignatory } from "./confirmation-secret.signatory";
-export { LedgerSignatory } from "./ledger.signatory";
-export { MnemonicSignatory } from "./mnemonic.signatory";
-export { PrivateKeySignatory } from "./private-key.signatory";
-export { SecretSignatory } from "./secret.signatory";
 export { Signatory } from "./signatory";
+export type { SignatoryData, SignatoryType } from "./signatory";
