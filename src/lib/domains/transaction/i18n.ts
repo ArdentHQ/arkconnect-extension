21cd1532af67d139a28fcb0481e252fb996b0cbc
Alfonso Bribiesca <alfonsobries@gmail.com>
2026-05-27 09:35:24 -0600

    style: quote i18next plural keys

--- src/lib/domains/transaction/i18n.ts
+++ src/lib/domains/transaction/i18n.ts
@@ -42,7 +42,7 @@ export const translations = {
     ENCRYPTION_PASSWORD: 'Encryption Password',
     ERROR: {
         DESCRIPTION:
-            "An error occurred while sending your transaction. Please go 'Back' and try again, or click 'Close' to return to the address.",
+            'An error occurred while sending your transaction. Please go \'Back\' and try again, or click \'Close\' to return to the address.',
         TITLE: 'Transaction Error',
     },
     EXPIRATION: {
@@ -50,7 +50,7 @@ export const translations = {
         TIMESTAMP: 'Timestamp Expiration',
     },
     EXPORT: {
-        DESCRIPTION: "Export your address' transaction history.",
+        DESCRIPTION: 'Export your address\' transaction history.',
         EMPTY: {
             DESCRIPTION: 'No transactions could be found for the selected period.',
         },
@@ -84,7 +84,7 @@ export const translations = {
             DESCRIPTION: '{{count}} transactions have been retrieved so far, please wait.',
             DESCRIPTION_START: 'The data is being prepared. This might take a while, please wait.',
             FETCHED_PARTIALLY:
-                "We've successfully retrieved some transactions, but encountered an issue along the way. You can download the partial file or retry.",
+                'We\'ve successfully retrieved some transactions, but encountered an issue along the way. You can download the partial file or retry.',
         },
         SUCCESS: {
             DESCRIPTION: '{{count}} transactions have been retrieved and are ready to be exported.',
@@ -185,7 +185,7 @@ export const translations = {
     },
     MODAL_QR_CODE: {
         DESCRIPTION:
-            "Hold a compatible QR code in front of your device's camera to scan the information.",
+            'Hold a compatible QR code in front of your device\'s camera to scan the information.',
         ERROR: 'Something went wrong.',
         INVALID_QR_CODE: 'The uploaded QR code is invalid.',
         PERMISSION_ERROR: {
@@ -216,8 +216,8 @@ export const translations = {
         AWAITING_CONFIRMATIONS: 'Awaiting confirmations',
         AWAITING_FINAL_SIGNATURE: 'Awaiting final signature',
         AWAITING_FINAL_SIGNATURE_AND_BROADCAST: 'Awaiting broadcast',
-        AWAITING_OTHER_SIGNATURE_COUNT_one: 'Awaiting {{count}} other signature',
-        AWAITING_OTHER_SIGNATURE_COUNT_other: 'Awaiting {{count}} other signatures',
+        'AWAITING_OTHER_SIGNATURE_COUNT_one': 'Awaiting {{count}} other signature',
+        'AWAITING_OTHER_SIGNATURE_COUNT_other': 'Awaiting {{count}} other signatures',
         AWAITING_OUR_BROADCAST: 'Awaiting our broadcast',
         AWAITING_OUR_SIGNATURE: 'Awaiting our signature',
         AWAITING_SOME_SIGNATURES: 'One or more transactions are awaiting our signature.',
@@ -275,7 +275,7 @@ export const translations = {
             DESCRIPTION: 'Register a new username address on the network below.',
             INFO: 'Username registrations are stored on-chain and publicly visible.',
             INFO_ALREADY_REGISTERED:
-                "This address is currently registered to the username <bold>'{{username}}'</bold>. Registering a new username will overwrite the existing one, as only one username is allowed per address.",
+                'This address is currently registered to the username <bold>\'{{username}}\'</bold>. Registering a new username will overwrite the existing one, as only one username is allowed per address.',
             TITLE: 'Username Registration',
         },
     },
@@ -337,7 +337,7 @@ export const translations = {
     REGISTRATION_TYPE: 'Registration Type',
     REJECTED_ERROR: {
         DESCRIPTION:
-            "You rejected the transaction on your Ledger device. Please go 'Back' and try again, or click 'Close' to return to the address.",
+            'You rejected the transaction on your Ledger device. Please go \'Back\' and try again, or click \'Close\' to return to the address.',
         TITLE: 'Transaction Rejected',
     },
     REPOSITORIES: {
@@ -443,7 +443,7 @@ export const translations = {
     TYPE: 'Type',
     UNVOTES: 'Unvotes',
     UNVOTES_COUNT: 'Unvotes ({{count}})',
-    UNVOTES_COUNT_one: 'Unvote',
+    'UNVOTES_COUNT_one': 'Unvote',
     VALIDATION: {
         ALREADY_VOTING: '{{wallet}} is already voting for {{validator}}.',
         AMOUNT_BELOW_MINIMUM: 'The amount is below the minimum ({{min}} {{ coinId }})',
@@ -486,7 +486,7 @@ export const translations = {
     VOTER: 'Voter',
     VOTES: 'Votes',
     VOTES_COUNT: 'Votes ({{count}})',
-    VOTES_COUNT_one: 'Vote',
+    'VOTES_COUNT_one': 'Vote',
     WAITING: 'Waiting',
     WEBSITE: 'Website',
     YOUR_ADDRESS: 'Your address',
