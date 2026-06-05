9e78d9af529eec0a81878ca404b93b0adbb7a196
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-06-05 01:19:56 +0100

    fix: restore fiat conversion in InputFeeSimple

--- src/lib/domains/transaction/components/InputFee/blocks/InputFeeSimple.tsx
+++ src/lib/domains/transaction/components/InputFee/blocks/InputFeeSimple.tsx
@@ -62,6 +62,28 @@ export const InputFeeSimple: React.FC<InputFeeSimpleProperties> = ({
 
                                     {!loading && (
                                         <div className='flex justify-between sm:w-full'>
+                                            {showConvertedValues && (
+                                                <span className='text-xs leading-[15px]'>
+                                                    ~
+                                                    <Amount
+                                                        ticker={exchangeTicker}
+                                                        value={displayValueConverted}
+                                                    />
+                                                </span>
+                                            )}
+                                            <span
+                                                className={cn(
+                                                    'text-theme-secondary-500 ml-1 text-xs leading-[15px] transition-all sm:hidden',
+                                                    {
+                                                        'dark:text-theme-dark-500 dim:text-theme-dim-200':
+                                                            !isSelected,
+                                                        'dim:text-theme-dim-navy-600': isSelected,
+                                                        hidden: !showConvertedValues,
+                                                    },
+                                                )}
+                                            >
+                                                (
+                                            </span>
                                             <Amount
                                                 ticker={ticker}
                                                 value={displayValue}
@@ -71,9 +93,24 @@ export const InputFeeSimple: React.FC<InputFeeSimpleProperties> = ({
                                                         'dark:group-hover:text-theme-dark-200 dark:text-theme-dark-500 dim:text-theme-dim-200 group-hover:dim:text-theme-dim-200':
                                                             !isSelected,
                                                         'dim:text-theme-dim-navy-600': isSelected,
+                                                        'text-theme-secondary-500':
+                                                            showConvertedValues,
                                                     },
                                                 )}
                                             />
+                                            <span
+                                                className={cn(
+                                                    'text-theme-secondary-500 text-xs leading-[15px] transition-all sm:hidden',
+                                                    {
+                                                        'dark:text-theme-dark-500 dim:text-theme-dim-200':
+                                                            !isSelected,
+                                                        'dim:text-theme-dim-navy-600': isSelected,
+                                                        hidden: !showConvertedValues,
+                                                    },
+                                                )}
+                                            >
+                                                )
+                                            </span>
                                         </div>
                                     )}
                                 </div>
@@ -88,95 +125,12 @@ export const InputFeeSimple: React.FC<InputFeeSimpleProperties> = ({
                                         },
                                     )}
                                 >
-                                    <div className='flex items-center justify-between p-3 sm:flex-col sm:items-start sm:justify-start sm:space-y-2'>
-                                        <div
-                                            className={cn('text-sm leading-[17px] sm:leading-5', {
-                                                'dark:group-hover:text-theme-dark-50 group-hover:dim:text-theme-dim-50 group-hover:text-theme-secondary-900':
-                                                    !isSelected,
-                                                'dark:text-theme-dark-50 dim:text-theme-dim-50 text-theme-navy-700':
-                                                    isSelected,
-                                            })}
-                                        >
-                                            {label}
-                                        </div>
-
-                                        {loading && <Skeleton width={100} className='h-4' />}
-
-                                        {!loading && (
-                                            <div className='flex justify-between sm:w-full'>
-                                                {showConvertedValues && (
-                                                    <span className='text-xs leading-[15px]'>
-                                                        ~
-                                                        <Amount
-                                                            ticker={exchangeTicker}
-                                                            value={displayValueConverted}
-                                                        />
-                                                    </span>
-                                                )}
-                                                <span
-                                                    className={cn(
-                                                        'text-theme-secondary-500 ml-1 text-xs leading-[15px] transition-all sm:hidden',
-                                                        {
-                                                            'dark:text-theme-dark-500 dim:text-theme-dim-200':
-                                                                !isSelected,
-                                                            'dim:text-theme-dim-navy-600':
-                                                                isSelected,
-                                                            hidden: !showConvertedValues,
-                                                        },
-                                                    )}
-                                                >
-                                                    (
-                                                </span>
-                                                <Amount
-                                                    ticker={ticker}
-                                                    value={displayValue}
-                                                    className={cn(
-                                                        'text-xs leading-[15px] transition-all',
-                                                        {
-                                                            'dark:group-hover:text-theme-dark-200 dark:text-theme-dark-500 dim:text-theme-dim-200 group-hover:dim:text-theme-dim-200':
-                                                                !isSelected,
-                                                            'dim:text-theme-dim-navy-600':
-                                                                isSelected,
-                                                            'text-theme-secondary-500':
-                                                                showConvertedValues,
-                                                        },
-                                                    )}
-                                                />
-                                                <span
-                                                    className={cn(
-                                                        'text-theme-secondary-500 text-xs leading-[15px] transition-all sm:hidden',
-                                                        {
-                                                            'dark:text-theme-dark-500 dim:text-theme-dim-200':
-                                                                !isSelected,
-                                                            'dim:text-theme-dim-navy-600':
-                                                                isSelected,
-                                                            hidden: !showConvertedValues,
-                                                        },
-                                                    )}
-                                                >
-                                                    )
-                                                </span>
-                                            </div>
-                                        )}
-                                    </div>
-                                    <div
-                                        className={cn(
-                                            'dark:text-theme-dark-200 text-theme-secondary-700 flex w-full justify-between px-3 py-2 text-xs leading-[15px] font-semibold transition-all',
-                                            {
-                                                'dark:bg-theme-dark-500 dim:bg-theme-dim-navy-900 dim:text-theme-dim-navy-400 bg-theme-navy-100':
-                                                    isSelected,
-                                                'dark:bg-theme-dark-800 dark:group-hover:bg-theme-dark-600 dark:group-hover:text-theme-dark-200 dim:bg-theme-dim-800 dim:text-theme-dim-200 group-hover:dim:bg-theme-dim-600 group-hover:dim:text-theme-dim-200 bg-theme-navy-50':
-                                                    !isSelected,
-                                            },
-                                        )}
-                                    >
-                                        <span>{t('COMMON.CONFIRMATION_TIME_LABEL')}</span>
-                                        <span>
-                                            {t('COMMON.CONFIRMATION_TIME', {
-                                                time: byFeeType(label),
-                                            }).toString()}
-                                        </span>
-                                    </div>
+                                    <span>{t('COMMON.CONFIRMATION_TIME_LABEL')}</span>
+                                    <span>
+                                        {t('COMMON.CONFIRMATION_TIME', {
+                                            time: byFeeType(label),
+                                        }).toString()}
+                                    </span>
                                 </div>
                             </div>
                         </ButtonGroupOption>
