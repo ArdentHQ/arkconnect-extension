4ad473092ac12363a43eb77a74c943ef5511193e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-07-10 03:47:56 +0100

    refactor: remove cryptocompare in favour of coingecko

--- src/lib/profiles/profile.importer.ts
+++ src/lib/profiles/profile.importer.ts
@@ -29,6 +29,10 @@ export class ProfileImporter implements IProfileImporter {
 
 		data = await this.#migrator.migrate(this.#profile, data);
 
+		if (data.settings?.["MARKET_PROVIDER"] === "cryptocompare") {
+			data.settings["MARKET_PROVIDER"] = "coingecko";
+		}
+
 		data = this.#validator.validate(data);
 
 		if (!this.#ignoreDetails) {
